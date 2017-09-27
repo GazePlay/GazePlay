75649d66a1982d0490c054ea4da831292fe23570
schwabdidier <didier.schwab@imag.fr>
2017-09-27 18:17:23 +0200

    work on tobii

--- tobii/Tobii.java
+++ tobii/Tobii.java
@@ -14,7 +14,7 @@ import java.nio.channels.ReadableByteChannel;
 
 public class Tobii {
 
-   /* private static final double screenWidth = com.sun.glass.ui.Screen.getScreens().get(0).getWidth();
+    private static final double screenWidth = com.sun.glass.ui.Screen.getScreens().get(0).getWidth();
     private static final double screenHeight = com.sun.glass.ui.Screen.getScreens().get(0).getWidth();
 
     private static Point2D parseTobiiOutput(String tobiiOutput) {
@@ -32,7 +32,7 @@ public class Tobii {
         Point2D point = new Point2D(x, y);
 
         return point;
-    }*/
+    }
 
     public static void oldExecProg(TobiiGazeListener listener) {
 
@@ -69,7 +69,7 @@ public class Tobii {
         }
     }
 
-    public static void execProg(TobiiGazeListener listener) {
+    public static void execProgold2(TobiiGazeListener listener) {
 
         Runtime runtime = Runtime.getRuntime();
 
@@ -102,16 +102,52 @@ public class Tobii {
         }
     }
 
+    public static void execProg(TobiiGazeListener listener) {
+        init();
+
+        while(true){
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(gazePosition());
+        }
+    }
+
+    public static native void init();
+
+    public static native String gazePosition();
+
+
+    static{
+
+        try {
+            System.loadLibrary("tobii_stream_engine");
+            System.loadLibrary("GazePlayTobiiLibrary2");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] argv){
 
-     /*   System.out.println(parseTobiiOutput("Gaze point: 10809754557 0.433731, 0.719170"));
-        System.out.println(parseTobiiOutput("Gaze point: 10809814527 0.372081, 0.670327"));
+        System.out.println(parseTobiiOutput("Gaze point: 10809754557 0.433731, 0.719170"));
+     /*   System.out.println(parseTobiiOutput("Gaze point: 10809814527 0.372081, 0.670327"));
         System.out.println(parseTobiiOutput("Gaze point: 10809828278 0.373068, 0.672786"));
         System.out.println(parseTobiiOutput("Gaze point: 10809874253 0.369580, 0.700685"));
         System.out.println(parseTobiiOutput("Gaze point: 10809889523 0.371530, 0.707418"));
         System.out.println(parseTobiiOutput("Gaze point: 10817493839 INVALID"));*/
 
-        execProg(null);
+        init();
+
+        while(true){
+
+            System.out.println(gazePosition());
+        }
+
 
     }
 
