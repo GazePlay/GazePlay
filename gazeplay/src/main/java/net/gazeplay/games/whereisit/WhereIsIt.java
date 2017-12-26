afc54c68597051d41cc5c0f82e564a956c37adef
Yann ROBERT <yann.robert@anantaplex.fr>
2017-12-26 17:41:29 +0100

    extracted method that compute game sizing

--- gazeplay/src/main/java/net/gazeplay/games/whereisit/WhereIsIt.java
+++ gazeplay/src/main/java/net/gazeplay/games/whereisit/WhereIsIt.java
@@ -22,6 +22,8 @@ import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -91,32 +93,43 @@ public class WhereIsIt {
         this.stats.setName(gameType.getGameName());
     }
 
-    public void buildGame() {
-        double shift = 0;
+    @Data
+    @AllArgsConstructor
+    private static class GameSizing {
+        private final double width;
+        private final double height;
+        private final double shift;
+    }
 
+    public GameSizing computeGameSizing() {
         Rectangle2D bounds = javafx.stage.Screen.getPrimary().getBounds();
 
         double screenWidth = bounds.getWidth();
         double screenHeight = bounds.getHeight();
 
-        double width = 0, height = 0;
+        final double width;
+        final double height;
+        final double shift;
 
         log.info("16/9 or 16/10 screen ? = " + ((screenWidth / screenHeight) - (16.0 / 9.0)));
 
         if (fourThree && ((screenWidth / screenHeight) - (16.0 / 9.0)) < 0.1) {
-
             width = 4 * screenHeight / 3;
             height = screenHeight;
             shift = (screenWidth - width) / 2;
         } else {
-
             width = screenWidth;
             height = screenHeight;
             shift = 0;
         }
 
-        width = width / nbColumns;
-        height = height / nbLines;
+        return new GameSizing(width / nbColumns, height / nbLines, shift);
+    }
+
+    public void buildGame() {
+
+        final GameSizing gameSizing = computeGameSizing();
+
         final int nbImages = nbLines * nbColumns;
         log.debug("nbImages = {}", nbImages);
         Random r = new Random();
@@ -171,8 +184,9 @@ public class WhereIsIt {
                 Utils.playSound(this.pathSound);
             }
 
-            Pictures picture = new Pictures(width * posX + shift, height * posY, width, height, group, scene,
-                    winner == i, randomImageFile + "", choiceBox, stats, this);
+            Pictures picture = new Pictures(gameSizing.width * posX + gameSizing.shift, gameSizing.height * posY,
+                    gameSizing.width, gameSizing.height, group, scene, winner == i, randomImageFile + "", choiceBox,
+                    stats, this);
 
             log.info("posX " + posX);
             log.info("posY " + posY);
