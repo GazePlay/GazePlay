8784b8ba89be838cfa60f62f6503c845444ec122
Yann ROBERT <yann.robert@anantaplex.fr>
2018-01-29 22:08:18 +0100

    fixes issue #181 : the blindfold size should match the size of the component it is hiding

--- gazeplay/src/main/java/net/gazeplay/GameContext.java
+++ gazeplay/src/main/java/net/gazeplay/GameContext.java
@@ -52,6 +52,8 @@ public class GameContext extends GraphicalContext<Pane> {
         menuHBox.toFront();
 
         Rectangle blindFoldPanel = new Rectangle(0, 0, 0, 0);
+        blindFoldPanel.widthProperty().bind(menuHBox.widthProperty());
+        blindFoldPanel.heightProperty().bind(menuHBox.heightProperty());
 
         StackPane bottomStackPane = new StackPane();
         bottomStackPane.getChildren().add(blindFoldPanel);
@@ -60,14 +62,12 @@ public class GameContext extends GraphicalContext<Pane> {
         EventHandler<MouseEvent> mouseEnterControlPanelEventHandler = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-                resizeBlindFoldPanel(blindFoldPanel, menuHBox);
                 blindFoldPanel.toBack();
             }
         };
         EventHandler<MouseEvent> mouseExitControlPanelEventHandler = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-                resizeBlindFoldPanel(blindFoldPanel, menuHBox);
                 blindFoldPanel.toFront();
             }
         };
