40b2e66c65367892a61a30583fc09f49ef03e412
Yann ROBERT <yann.robert@anantaplex.fr>
2018-01-31 00:20:30 +0100

    clean up code for the CreamPie game. Happens to have solve issue #156

--- gazeplay/src/main/java/net/gazeplay/games/creampie/CreamPie.java
+++ gazeplay/src/main/java/net/gazeplay/games/creampie/CreamPie.java
@@ -1,38 +1,51 @@
 package net.gazeplay.games.creampie;
 
-/**
- * Created by schwab on 12/08/2016.
- */
-
+import javafx.scene.image.Image;
 import net.gazeplay.GameContext;
 import net.gazeplay.GameLifeCycle;
 import net.gazeplay.commons.utils.Portrait;
-import net.gazeplay.commons.utils.stats.ShootGamesStats;
+import net.gazeplay.commons.utils.RandomPositionGenerator;
 import net.gazeplay.commons.utils.stats.Stats;
 
+/**
+ * Created by schwab on 12/08/2016.
+ */
 public class CreamPie implements GameLifeCycle {
 
     private final GameContext gameContext;
 
     private final Stats stats;
 
+    private final Hand hand;
+
+    private final Target portrait;
+
     public CreamPie(GameContext gameContext, Stats stats) {
         super();
         this.gameContext = gameContext;
         this.stats = stats;
+
+        final Image[] availableImages = Portrait.loadAllImages();
+        final RandomPositionGenerator randomPositionGenerator = gameContext.getRandomPositionGenerator();
+
+        hand = new Hand();
+        portrait = new Target(randomPositionGenerator, hand, stats, gameContext, availableImages);
+
+        gameContext.getChildren().add(portrait);
+        gameContext.getChildren().add(hand);
     }
 
     @Override
     public void launch() {
-        Hand hand = new Hand();
-
-        Target portrait = new Target(gameContext.getRandomPositionGenerator(), hand, stats, gameContext,
-                Portrait.loadAllImages());
-
-        gameContext.getChildren().add(portrait);
-        gameContext.getChildren().add(hand);
-
         hand.recomputePosition();
+
+        gameContext.getRoot().widthProperty().addListener((obs, oldVal, newVal) -> {
+            hand.recomputePosition();
+        });
+        gameContext.getRoot().heightProperty().addListener((obs, oldVal, newVal) -> {
+            hand.recomputePosition();
+        });
+
     }
 
     @Override
