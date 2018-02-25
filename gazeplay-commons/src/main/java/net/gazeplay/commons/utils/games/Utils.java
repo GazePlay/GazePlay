54e671d32ee1159518230131e58e1090b9ed121d
Didier Schwab <didier.schwab@gmail.com>
2018-02-25 15:12:53 +0100

    Revert "DO NOT MERGE YET : introduce background music"

--- gazeplay-commons/src/main/java/net/gazeplay/commons/utils/games/Utils.java
+++ gazeplay-commons/src/main/java/net/gazeplay/commons/utils/games/Utils.java
@@ -1,6 +1,5 @@
 package net.gazeplay.commons.utils.games;
 
-import javafx.application.Platform;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -10,18 +9,15 @@ import javafx.scene.media.MediaPlayer;
 import lombok.extern.slf4j.Slf4j;
 import net.gazeplay.commons.configuration.Configuration;
 import net.gazeplay.commons.configuration.ConfigurationBuilder;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
 import javax.activation.MimetypesFileTypeMap;
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.Date;
 import java.util.Enumeration;
 
@@ -169,73 +165,6 @@ public class Utils {
         defaultImages[9] = new Image(ClassLoader.getSystemResourceAsStream(
                 "data/common/default/images/pez-payaso-animales-el-mar-pintado-por-teoalmeyra-9844979.jpg"));
         return defaultImages;
-    }
-
-    public static void playRemoteSound(String resourceUrlAsString) {
-
-        Runnable asyncTask = new Runnable() {
-            @Override
-            public void run() {
-                // parse the URL early
-                // in order to fail early if the URL is invalid
-                URL resourceURL = null;
-                try {
-                    resourceURL = new URL(resourceUrlAsString);
-                } catch (MalformedURLException e) {
-                    throw new RuntimeException("Invalid URL provided as sound resource : " + resourceUrlAsString, e);
-                }
-
-                // the local cache filename is a Base64 encoding of the URL
-                // so that we avoid name clash,
-                // and so that we have the same local file for the same resource URL
-                final Charset utf8 = Charset.forName("UTF-8");
-                byte[] encodedUrl = Base64.getEncoder().encode(resourceURL.toExternalForm().getBytes(utf8));
-                final String localCacheFileName = new String(encodedUrl, utf8);
-
-                File musicCacheFolder = new File("cache/music");
-                File outputFile = new File(musicCacheFolder, localCacheFileName);
-
-                if (!outputFile.exists()) {
-                    // use a temporary file while downloading
-                    // to avoid using corrupted file is the download is interrupted
-                    File tempOutputFile = new File(outputFile.getAbsolutePath() + ".downloading");
-                    if (tempOutputFile.exists()) {
-                        boolean tempOutputFileDeleted = tempOutputFile.delete();
-                        log.trace("tempOutputFileDeleted = {}", tempOutputFileDeleted);
-                    }
-                    try {
-                        log.info("Downloading music file {}", resourceURL);
-
-                        FileUtils.copyURLToFile(resourceURL, tempOutputFile, 10000, 10000);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    tempOutputFile.renameTo(outputFile);
-                    log.info("Finished downloading music file {}", resourceURL);
-                } else {
-                    log.info("Found music file in cache for {}", resourceURL);
-                }
-
-                Platform.runLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        final String localResourceName = outputFile.toURI().toString();
-                        log.info("Playing sound {}", localResourceName);
-                        try {
-                            Media media = new Media(localResourceName);
-                            MediaPlayer mp = new MediaPlayer(media);
-                            mp.setCycleCount(Integer.MAX_VALUE);
-                            mp.play();
-                        } catch (RuntimeException e) {
-                            log.error("Exception while playing media file {}", localResourceName, e);
-                        }
-                    }
-                });
-            }
-        };
-
-        Thread thread = new Thread(asyncTask);
-        thread.start();
     }
 
     public static void playSound(String ressource) {
