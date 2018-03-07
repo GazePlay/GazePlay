707c0c6e212f8805dfda82d36a6ae777dbcb982e
kuhlkrein <superseb.07@hotmail.fr>
2018-03-07 11:20:23 +0100

    End of the Biboules and Robots (Hand motion) + EyeTraking ok

--- gazeplay/src/main/java/net/gazeplay/games/shooter/Target.java
+++ gazeplay/src/main/java/net/gazeplay/games/shooter/Target.java
@@ -1,5 +1,6 @@
 package net.gazeplay.games.shooter;
 
+import javafx.animation.ScaleTransition;
 import javafx.animation.Transition;
 import javafx.scene.layout.StackPane;
 import net.gazeplay.games.shooter.Point;
@@ -9,9 +10,13 @@ public class Target extends StackPane {
     Point destination;
     Transition t;
     double centerX, centerY;
+    Boolean done;
+    Boolean animDone;
 
     Target() {
         super();
+        animDone = true;
+        done = false;
 
     }
 
