3d22e0274559bec2cae57ba9b4523ce32c16a36f
Thomas MEDARD <medard.thomas@outlook.fr>
2018-03-07 11:42:25 +0100

    Use JavaFX instead of AWT

--- gazeplay/src/main/java/net/gazeplay/games/colors/ColorsGame.java
+++ gazeplay/src/main/java/net/gazeplay/games/colors/ColorsGame.java
@@ -7,6 +7,9 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Node;
 import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -49,7 +52,7 @@ public class ColorsGame implements GameLifeCycle {
         double height = dimension2D.getHeight();
 
         buildToolBox(width, height);
-        buildDraw(width, height);
+        buildDraw(DEFAULT_IMAGE_URL, width, height);
     }
 
     @Override
@@ -69,34 +72,132 @@ public class ColorsGame implements GameLifeCycle {
         colorToolBox.relocate(x, y);
     }
 
-    private void buildDraw(double width, double height) {
+    private void buildDraw(String imgURL, double width, double height) {
 
         // TODO
 
-        Rectangle testRect = new Rectangle(width, height);
+        Rectangle rectangle = new Rectangle(width, height);
 
-        Image img = new Image(DEFAULT_IMAGE_URL, width, height, false, true);
+        Image img = new Image(imgURL, width, height, false, true);
+        
+        //awtEditing(img, rectangle);
+        javaFXEditing(img, rectangle);
+        
+        root.getChildren().add(rectangle);
+
+        rectangle.toBack();
+    }
+    
+    public void javaFXEditing(Image image, Rectangle rectangle) {
+        
+        rectangle.setFill(new ImagePattern(image));
+        
+        final PixelReader tmpPixelReader = image.getPixelReader();
+        
+        if(tmpPixelReader == null) {
+            log.info("Error in image loading : ");
+            log.error("Colors : unable to read pixels from image");
+            return;
+        }
+        
+        final WritableImage writableImg = new WritableImage(tmpPixelReader, (int)image.getWidth(), (int)image.getHeight());
+        final PixelWriter pixelWriter = writableImg.getPixelWriter();
+        final PixelReader pixelReader = writableImg.getPixelReader();
+        
+        log.info("Pixel format = {}", pixelReader.getPixelFormat());
+        
+         rectangle.addEventFilter(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
+             
+            log.info("clicked : {}", event.getSceneX(), event.getSceneY());
+
+            Color color = pixelReader.getColor((int)event.getSceneX(), (int)event.getSceneY());
+            log.info("R = {}, G = {}, B = {}, A = {}", color.getRed(), color.getGreen(), color.getBlue(), color.getOpacity());
+            
+             javaFXFloodFill(pixelWriter, pixelReader, colorToolBox.getSelectedColorBox().getColor(), 
+                     (int)event.getSceneX(), (int)event.getSceneY(), (int)image.getWidth(), (int) image.getHeight());
+             
+             rectangle.setFill(new ImagePattern(writableImg));
+        });
+    }
+    
+    public void javaFXFloodFill(final PixelWriter pixelWriter, final PixelReader pixelReader, 
+            Color newColor, int x, int y, int width, int height) {
+        
+        final Color oldColor = pixelReader.getColor(x, y);
+        
+        floodInColumnAndLine(pixelWriter, pixelReader, newColor, x, y, width,
+                height, oldColor);
+        //floodInColumnAndLine(pixelWriter, pixelReader, newColor, x, y, width, height, false);
+    }
+    
+    public void floodInColumnAndLine(final PixelWriter pixelWriter, 
+            final PixelReader pixelReader, final Color newColor, final int x, 
+            final int y, final int width, final int height, 
+            final Color oldColor) {
+
+        int fillL = floodInLine(pixelWriter, pixelReader, newColor, x, y, width, true, oldColor);
+        int fillR = floodInLine(pixelWriter, pixelReader, newColor, x, y, width, false, oldColor);
+        
+        log.info("fillL = {}, fillR = {}", fillL, fillR);
+
+        // checks if applicable up or down
+        for (int i = fillL; i <= fillR; i++) {
+            if (y > 0 && isEqualColors(pixelReader.getColor(i, y - 1), oldColor))
+                floodInColumnAndLine(pixelWriter, pixelReader, newColor, i, y - 1, width, height, oldColor);
+            if (y < height - 1 && isEqualColors(pixelReader.getColor(i, y + 1), oldColor))
+                floodInColumnAndLine(pixelWriter, pixelReader, newColor, i, y + 1, width, height, oldColor);
+        }
+    }
+    
+    public int floodInLine(final PixelWriter pixelWriter, 
+            final PixelReader pixelReader, final Color newColor, final int x, 
+            final int y, final int width, final boolean isLeftFIll, 
+            final Color oldColor) {
+        
+            int currentX = x;
+
+            // Right fill
+            do {
+                
+                pixelWriter.setColor(currentX, y, newColor);
+                
+                if(isLeftFIll) 
+                    currentX--;
+                else 
+                    currentX++;
+            } while(currentX >= 0 && currentX < width - 1 && 
+                    isEqualColors(pixelReader.getColor(currentX, y), oldColor));
+            
+            if(isLeftFIll) 
+                currentX++;
+            else {
+
+                currentX--;
+            }
+            
+            return currentX;
+    }
+    
+    public void awtEditing(Image img, Rectangle rectangle) {
         BufferedImage buffImg = SwingFXUtils.fromFXImage(img, null);
+        
         if (buffImg == null) {
             log.info("Unable to write into image");
         }
-        testRect.setFill(new ImagePattern(img));
+        rectangle.setFill(new ImagePattern(img));
         // testRect.setFill(Color.RED);
-        testRect.addEventFilter(MouseEvent.MOUSE_CLICKED, (event) -> {
+        rectangle.addEventFilter(MouseEvent.MOUSE_CLICKED, (event) -> {
 
             Point loc = new Point((int) event.getSceneX(), (int) event.getSceneY());
 
             log.info("clicked : {}", loc.getX(), loc.getY());
 
-            floodFill(buffImg, colorToolBox.getSelectedColorBox().getColor(), loc);
+            awtFloodFill(buffImg, colorToolBox.getSelectedColorBox().getColor(), loc);
 
             Image newImg = SwingFXUtils.toFXImage(buffImg, null);
-            testRect.setFill(new ImagePattern(newImg));
+            rectangle.setFill(new ImagePattern(newImg));
         });
-
-        root.getChildren().add(testRect);
-
-        testRect.toBack();
+        
     }
 
     /**
@@ -112,7 +213,7 @@ public class ColorsGame implements GameLifeCycle {
      *             if loc is out of bounds of the image
      * @see http://www.codecodex.com/wiki/Implementing_the_flood_fill_algorithm
      */
-    public static void floodFill(BufferedImage img, Color fillColor, Point loc) {
+    public static void awtFloodFill(BufferedImage img, Color fillColor, Point loc) {
         if (loc.x < 0 || loc.x >= img.getWidth() || loc.y < 0 || loc.y >= img.getHeight())
             throw new IllegalArgumentException();
 
@@ -122,8 +223,8 @@ public class ColorsGame implements GameLifeCycle {
         int[] old = raster.getPixel(loc.x, loc.y, new int[4]);
         old[3] = (int) fillColor.getOpacity() * 255;
 
-        log.info("Color = {}", fill);
-        log.info("R = {}, G={},B={}", (int) (fillColor.getRed() * 255), fillColor.getGreen(), fillColor.getBlue());
+        // log.info("Color = {}", fill);
+        // log.info("R = {}, G={},B={}", (int) (fillColor.getRed() * 255), fillColor.getGreen(), fillColor.getBlue());
 
         // Checks trivial case where loc is of the fill color
         if (isEqualRgba(fill, old))
@@ -188,5 +289,14 @@ public class ColorsGame implements GameLifeCycle {
     private static boolean isEqualRgba(int[] pix1, int[] pix2) {
 
         return pix1[0] == pix2[0] && pix1[1] == pix2[1] && pix1[2] == pix2[2] && pix1[3] == pix2[3];
+    }
+    
+    private static boolean isEqualColors(final Color color1, final Color color2) {
+        
+        if(color1.equals(color2)) {
+            return true;
+        }
+        
+        return false;
     }
 }
