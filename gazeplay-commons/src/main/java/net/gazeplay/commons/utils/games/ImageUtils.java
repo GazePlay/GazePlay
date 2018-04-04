525e1be93ff0f64e1e770ce7a177c771544b3a92
Yann ROBERT <yann.robert@anantaplex.fr>
2018-04-05 01:07:48 +0200

    ImagesUtils : implementation details : use List instead of Array

--- gazeplay-commons/src/main/java/net/gazeplay/commons/utils/games/ImageUtils.java
+++ gazeplay-commons/src/main/java/net/gazeplay/commons/utils/games/ImageUtils.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import javax.activation.MimetypesFileTypeMap;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class ImageUtils {
@@ -14,33 +15,30 @@ public class ImageUtils {
 
     public static Image[] loadAllImagesInDirectory(String imagesDirectoryPath) {
         final File directoryFile = new File(imagesDirectoryPath);
-        return loadAllImagesInDirectory(directoryFile);
+        List<Image> images = loadAllImagesInDirectory(directoryFile);
+        return images.toArray(new Image[images.size()]);
     }
 
-    private static Image[] loadAllImagesInDirectory(File directoryFile) {
+    private static List<Image> loadAllImagesInDirectory(File directoryFile) {
         log.info("Try to find images in folder : {}", directoryFile);
         if (directoryFile.exists()) {
-            Image[] images = getImages(directoryFile);
+            List<Image> images = getImages(directoryFile);
 
-            if (images.length != 0) {
-                log.debug("I found {} images in folder : {}", images.length, directoryFile);
+            if (!images.isEmpty()) {
+                log.debug("I found {} images in folder : {}", images.size(), directoryFile);
                 return images;
             } else {
                 log.info("No image in folder : {}", directoryFile);
-                return defaultImage();
+                return defaultImages();
             }
         } else {
             log.info("Folder doesn't exist : {}", directoryFile);
-            return defaultImage();
+            return defaultImages();
         }
     }
 
-    private static Image[] getImages(File directoryFile) {
-        return getImages(directoryFile, -1);
-    }
-
-    private static Image[] getImages(final File directoryFile, final int nbMax) {
-        ArrayList<Image> images = new ArrayList<>();
+    private static List<Image> getImages(File directoryFile) {
+        List<Image> result = new ArrayList<>();
 
         final String[] filenames = directoryFile.list();
         for (String imagePath : filenames) {
@@ -48,7 +46,7 @@ public class ImageUtils {
             boolean added;
             if (!imagePath.startsWith(".") && isImage(fileUrl)) { // Problems with filenames starting with a point on
                 // Windows
-                images.add(new Image(fileUrl));
+                result.add(new Image(fileUrl));
                 added = true;
             } else {
                 added = false;
@@ -56,57 +54,30 @@ public class ImageUtils {
             log.debug("{} : added = {}", fileUrl, added);
         }
 
-        Image[] Timages = obj2im(images.toArray());
-
-        if (nbMax <= 0) {
-            return Timages;
-        }
-
-        Image[] rimages = new Image[nbMax];
-
-        for (int i = 0; i < nbMax; i++) {
-            rimages[i] = Timages[(int) (Math.random() * Timages.length)];
-        }
-
-        return rimages;
+        return result;
     }
 
-    private static Image[] defaultImage() {
-
-        Image[] defaultImages = new Image[10];
-        defaultImages[0] = new Image(
-                ClassLoader.getSystemResourceAsStream("data/common/default/images/animal-807308_1920.png"));
-        defaultImages[1] = new Image(
-                ClassLoader.getSystemResourceAsStream("data/common/default/images/bulldog-1047518_1920.jpg"));
-        defaultImages[2] = new Image(
-                ClassLoader.getSystemResourceAsStream("data/common/default/images/businessman-607786_1920.png"));
-        defaultImages[3] = new Image(
-                ClassLoader.getSystemResourceAsStream("data/common/default/images/businessman-607834_1920.png"));
-        defaultImages[4] = new Image(
-                ClassLoader.getSystemResourceAsStream("data/common/default/images/crocodile-614386_1920.png"));
-        defaultImages[5] = new Image(
-                ClassLoader.getSystemResourceAsStream("data/common/default/images/goldfish-30837_1280.png"));
-        defaultImages[6] = new Image(
-                ClassLoader.getSystemResourceAsStream("data/common/default/images/graphic_missbone17.gif"));
-        defaultImages[7] = new Image(
-                ClassLoader.getSystemResourceAsStream("data/common/default/images/nurse-37322_1280.png"));
-        defaultImages[8] = new Image(
-                ClassLoader.getSystemResourceAsStream("data/common/default/images/owl-161583_1280.png"));
-        defaultImages[9] = new Image(ClassLoader.getSystemResourceAsStream(
-                "data/common/default/images/pez-payaso-animales-el-mar-pintado-por-teoalmeyra-9844979.jpg"));
-        return defaultImages;
-    }
-
-    private static Image[] obj2im(Object[] objects) {
-
-        Image[] images = new Image[objects.length];
-
-        for (int i = 0; i < objects.length; i++) {
-
-            images[i] = (Image) objects[i];
-        }
-
-        return images;
+    private static List<Image> defaultImages() {
+        List<Image> result = new ArrayList<>();
+        result.add(
+                new Image(ClassLoader.getSystemResourceAsStream("data/common/default/images/animal-807308_1920.png")));
+        result.add(new Image(
+                ClassLoader.getSystemResourceAsStream("data/common/default/images/bulldog-1047518_1920.jpg")));
+        result.add(new Image(
+                ClassLoader.getSystemResourceAsStream("data/common/default/images/businessman-607786_1920.png")));
+        result.add(new Image(
+                ClassLoader.getSystemResourceAsStream("data/common/default/images/businessman-607834_1920.png")));
+        result.add(new Image(
+                ClassLoader.getSystemResourceAsStream("data/common/default/images/crocodile-614386_1920.png")));
+        result.add(
+                new Image(ClassLoader.getSystemResourceAsStream("data/common/default/images/goldfish-30837_1280.png")));
+        result.add(
+                new Image(ClassLoader.getSystemResourceAsStream("data/common/default/images/graphic_missbone17.gif")));
+        result.add(new Image(ClassLoader.getSystemResourceAsStream("data/common/default/images/nurse-37322_1280.png")));
+        result.add(new Image(ClassLoader.getSystemResourceAsStream("data/common/default/images/owl-161583_1280.png")));
+        result.add(new Image(ClassLoader.getSystemResourceAsStream(
+                "data/common/default/images/pez-payaso-animales-el-mar-pintado-por-teoalmeyra-9844979.jpg")));
+        return result;
     }
 
     private static boolean isImage(String file) {
