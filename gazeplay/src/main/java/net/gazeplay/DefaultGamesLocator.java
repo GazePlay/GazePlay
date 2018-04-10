857546e3b0c73dd3617eb97f9071a898c00f97b4
DMyTryC <dmytryc@gmail.com>
2018-04-10 07:43:33 +0200

    Recreated the room game from the master branch of the gazeplay project

--- gazeplay/src/main/java/net/gazeplay/DefaultGamesLocator.java
+++ gazeplay/src/main/java/net/gazeplay/DefaultGamesLocator.java
@@ -34,6 +34,8 @@ import net.gazeplay.games.moles.MoleStats;
 import net.gazeplay.games.moles.Moles;
 import net.gazeplay.games.order.OrderStats;
 import net.gazeplay.games.order.Order;
+import net.gazeplay.games.room.Room;
+import net.gazeplay.games.room.RoomStats;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -98,15 +100,18 @@ public class DefaultGamesLocator implements GamesLocator {
                     }
                 }));
 
-        /*
-         * result.add(new GameSpec(new GameSummary("Room", DEFAULT_SEARCHING_GAME_THUMBNAIL), new
-         * GameSpec.GameLauncher() {
-         * 
-         * @Override public Stats createNewStats(Scene scene) { return new RoomStats(scene); }
-         * 
-         * @Override public GameLifeCycle createNewGame(GameContext gameContext, GameSpec.GameVariant gameVariant, Stats
-         * stats) { return new Room(gameContext, stats); } }));
-         */
+        result.add(new GameSpec(new GameSummary("Room", DEFAULT_SEARCHING_GAME_THUMBNAIL), new GameSpec.GameLauncher() {
+
+            @Override
+            public Stats createNewStats(Scene scene) {
+                return new RoomStats(scene);
+            }
+
+            @Override
+            public GameLifeCycle createNewGame(GameContext gameContext, GameSpec.GameVariant gameVariant, Stats stats) {
+                return new Room(gameContext, stats);
+            }
+        }));
 
         result.add(new GameSpec(
                 new GameSummary("MagicCards", DEFAULT_SEARCHING_GAME_THUMBNAIL, "data/Thumbnails/magic-card-1.jpg"),
