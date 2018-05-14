c3aae58f7fc1d99f49f45a0ce3efbf89844ddfb0
DMyTryC <dmytryc@gmail.com>
2018-05-14 10:41:02 +0200

    Refractoring code

--- gazeplay/src/main/java/net/gazeplay/games/room/Wall.java
+++ gazeplay/src/main/java/net/gazeplay/games/room/Wall.java
@@ -10,23 +10,21 @@ import lombok.extern.slf4j.Slf4j;
 public class Wall {
 
     // The short size of a side of the cube (this is the thickness of each side)
-    private static final double thickness = 0.1;
+    private double wallThickness;
 
     // The positions of the sides compared to the origin
     private double positionWall;
 
-    // Image rightImage = new Image("data/room/front.jpg", WIDTH, HEIGHT, true, true);
     Box box;
     String name;
 
     public Wall(String moveDirection, int positiveAxisMultiplier, String name, double xLength, double yLength) {
+        wallThickness = 0.1;
         this.name = name;
         switch (moveDirection) {
         case "X":
-            this.box = new Box(thickness, yLength, xLength);
+            this.box = new Box(wallThickness, yLength, xLength);
             this.positionWall = xLength / 2;
-            // this.box.setMaterial(new PhongMaterial(Color.TRANSPARENT, rightImage, rightImage, rightImage,
-            // rightImage));
             if (positiveAxisMultiplier == 1) {
                 Image rightImage = new Image("data/room/right.jpg");
                 this.box.setMaterial(
@@ -38,7 +36,7 @@ public class Wall {
             this.box.setTranslateX(positiveAxisMultiplier * positionWall);
             break;
         case "Y":
-            this.box = new Box(xLength, thickness, xLength);
+            this.box = new Box(xLength, wallThickness, xLength);
             this.positionWall = yLength / 2;
             Image top_bottomImage = new Image("data/room/top_bottom.jpg");
             this.box.setMaterial(new PhongMaterial(Color.TRANSPARENT, top_bottomImage, top_bottomImage, top_bottomImage,
@@ -46,7 +44,7 @@ public class Wall {
             this.box.setTranslateY(positiveAxisMultiplier * positionWall);
             break;
         case "Z":
-            this.box = new Box(xLength, yLength, thickness);
+            this.box = new Box(xLength, yLength, wallThickness);
             this.positionWall = xLength / 2;
             if (positiveAxisMultiplier == 1) {
                 Image frontImage = new Image("data/room/front.jpg");
