ba904fb0a6a78e3255bf6417116f2c6ecad04a88
kuhlkrein <superseb.07@hotmail.fr>
2018-07-19 13:19:24 +0200

    Next steps of the rushhour game

--- gazeplay/src/main/java/net/gazeplay/games/rushHour/Car.java
+++ gazeplay/src/main/java/net/gazeplay/games/rushHour/Car.java
@@ -12,6 +12,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import net.gazeplay.DefaultGamesLocator;
 import net.gazeplay.GameContext;
@@ -25,9 +26,11 @@ public class Car extends Rectangle {
     private boolean direction;
     private int size;
     private int move = 0;
+    @Setter
+    private boolean intersect = false;
     GameContext gameContext;
 
-    Car(int l, int h, Color c, boolean direction, Pane city, int size, GameContext gameContext) {
+    Car(int l, int h, Color c, boolean direction, int size, GameContext gameContext) {
         super(0, 0, l * size, h * size);
         this.setFill(c);
         this.gameContext = gameContext;
@@ -47,16 +50,21 @@ public class Car extends Rectangle {
         EventHandler<Event> exitEvent = new EventHandler<Event>() {
             @Override
             public void handle(Event e) {
-                if (selected) {
-                    if (e.getEventType() == MouseEvent.MOUSE_EXITED) {
-                        Point mouse = MouseInfo.getPointerInfo().getLocation();
-                        int way;
-                        if (0 != (way = checkPos(mouse))) {
-                            moveTo(city, way);
-                        }
+                boolean out = false;
+
+                Point mouse = MouseInfo.getPointerInfo().getLocation();
+
+                if (e.getEventType() == MouseEvent.MOUSE_EXITED) {
+                    int way = checkPos(mouse);
+                    selected = false;
+                    log.info("unselected");
+
+                    if (!out && !intersect && !onMouse(mouse)) {
+                        // moveToMouse(mouse);
+                        moveTo(way, mouse);
                         selected = false;
-                        log.info("unselected");
                     }
+                    intersect = false;
                 }
             }
         };
@@ -65,42 +73,81 @@ public class Car extends Rectangle {
 
     }
 
-    private Car getInstance() {
-        return this;
-    }
-
-    private void checkWin() {
-
-    }
-
     private int checkPos(Point mouse) {
-        Point2D coord = this.sceneToLocal(mouse.getX() - gameContext.getGazePlay().getPrimaryStage().getX(),
-                mouse.getY() - gameContext.getGazePlay().getPrimaryStage().getY());
+        Point2D coord = this.sceneToLocal(
+                mouse.getX() - gameContext.getGazePlay().getPrimaryScene().getX()
+                        - gameContext.getGazePlay().getPrimaryStage().getX(),
+                mouse.getY() - gameContext.getGazePlay().getPrimaryScene().getY()
+                        - gameContext.getGazePlay().getPrimaryStage().getY());
 
-        if (!direction && (this.getX() <= coord.getX()) && (coord.getX() <= this.getX() + this.getWidth())) {
-            return (this.getY() < coord.getY()) ? 1 : -1;
-        } else if (direction && (this.getY() <= coord.getY()) && (coord.getY() <= this.getY() + this.getHeight())) {
-            return (this.getX() < coord.getX()) ? 1 : -1;
+        if (!direction && (this.getX() < coord.getX()) && (coord.getX() < this.getX() + this.getWidth())) {
+            return (this.getY() > coord.getY()) ? -1 : 1;
+        } else if (direction && (this.getY() < coord.getY()) && (coord.getY() < this.getY() + this.getHeight())) {
+            return (this.getX() > coord.getX()) ? -1 : 1;
         }
+
+        log.info("car (x;y) = (" + this.getX() + ";" + this.getY() + ")");
+        log.info("car offset (x;y) = (" + (this.getX() + this.getWidth()) + ";" + (this.getY() + this.getHeight())
+                + ")");
+        log.info("mouse (x;y) = (" + coord.getX() + ";" + coord.getY() + ")");
 
         return 0;
     }
 
-    private boolean isInside(Point mouse) {
-        Point2D coord = this.sceneToLocal(mouse.getX() - gameContext.getGazePlay().getPrimaryStage().getX(),
-                mouse.getY() - gameContext.getGazePlay().getPrimaryStage().getY());
-        boolean b = (this.getX() - 5 <= coord.getX()) && (coord.getX() <= 5 + this.getX() + this.getWidth())
-                && (this.getY() - 5 <= coord.getY()) && (coord.getY() <= 5 + this.getY() + this.getHeight());
-        log.info("" + b);
-        return b;
+    private void moveTo(int way, Point mouse) {
+        Point2D coord = this.sceneToLocal(
+                mouse.getX() - gameContext.getGazePlay().getPrimaryScene().getX()
+                        - gameContext.getGazePlay().getPrimaryStage().getX(),
+                mouse.getY() - gameContext.getGazePlay().getPrimaryScene().getY()
+                        - gameContext.getGazePlay().getPrimaryStage().getY());
+        double saveX = this.getX();
+        double saveY = this.getY();
+        if (this.isDirection()) {
+            this.setX(this.getX() + way * size);
+        } else {
+            this.setY(this.getY() + way * size);
+        }
+        if (intersect) {
+            this.setX(saveX);
+            this.setY(saveY);
+        }
     }
 
-    private void moveTo(Pane city, int way) {
+    private void moveToMouse(Point mouse) {
+        Point2D coord = this.sceneToLocal(
+                mouse.getX() - gameContext.getGazePlay().getPrimaryScene().getX()
+                        - gameContext.getGazePlay().getPrimaryStage().getX(),
+                mouse.getY() - gameContext.getGazePlay().getPrimaryScene().getY()
+                        - gameContext.getGazePlay().getPrimaryStage().getY());
+        double saveX = this.getX();
+        double saveY = this.getY();
         if (this.isDirection()) {
-            this.setX(this.getX() + size);
+            this.setX(((int) coord.getX() / size) * size);
         } else {
-            this.setY(this.getY() + size);
+            this.setY(((int) coord.getY() / size) * size);
         }
+        if (intersect) {
+            this.setX(saveX);
+            this.setY(saveY);
+        }
+    }
+
+    private boolean onMouse(Point mouse) {
+        Point2D coord = this.sceneToLocal(
+                mouse.getX() - gameContext.getGazePlay().getPrimaryScene().getX()
+                        - gameContext.getGazePlay().getPrimaryStage().getX(),
+                mouse.getY() - gameContext.getGazePlay().getPrimaryScene().getY()
+                        - gameContext.getGazePlay().getPrimaryStage().getY());
+        return (this.getX() < coord.getX()) && (coord.getX() < this.getX() + this.getWidth())
+                && (this.getY() < coord.getY()) && (coord.getY() < this.getY() + this.getHeight());
+    }
+
+    public void setToX(int i) {
+        super.setX(size + i * size);
+    }
+
+    public void setToY(int i) {
+        super.setY(size + i * size);
     }
 
 }
--- gazeplay/src/main/java/net/gazeplay/games/rushHour/RushHour.java
+++ gazeplay/src/main/java/net/gazeplay/games/rushHour/RushHour.java
@@ -1,9 +1,14 @@
 package net.gazeplay.games.rushHour;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javafx.scene.Parent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import lombok.extern.slf4j.Slf4j;
 import net.gazeplay.DefaultGamesLocator;
 import net.gazeplay.GameContext;
@@ -12,23 +17,37 @@ import net.gazeplay.GameLifeCycle;
 @Slf4j
 public class RushHour extends Parent implements GameLifeCycle {
 
-    public Pane city;
     public GameContext gameContext;
+    public int size = 100;
+
+    public List<Car> garage;
+    public Shape walls;
 
     public RushHour(GameContext gameContext) {
         this.gameContext = gameContext;
-        city = new Pane();
         setLevel(0);
 
-        gameContext.getChildren().add(city);
     }
 
     public void setLevel(int i) {
 
-        Car red = new Car(1, 2, Color.RED, true, city, 50, gameContext);
-        red.setX(0);
-        red.setY(0);
-        city.getChildren().add(red);
+        garage = new LinkedList<Car>();
+
+        Car red = new Car(1, 2, Color.RED, true, size, gameContext);
+        red.setToX(0);
+        red.setToY(0);
+        garage.add(red);
+        gameContext.getChildren().add(red);
+
+        Car blue = new Car(2, 2, Color.BLUE, false, size, gameContext);
+        blue.setToX(4);
+        blue.setToY(5);
+        garage.add(blue);
+        gameContext.getChildren().add(blue);
+
+        createGarage(10, 6);
+
+        setIntersections();
 
     }
 
@@ -42,6 +61,53 @@ public class RushHour extends Parent implements GameLifeCycle {
     public void dispose() {
         // TODO Auto-generated method stub
 
+    }
+
+    public void setIntersections() {
+        for (Car car : garage) {
+            if (car.isDirection()) {
+                car.xProperty().addListener((o) -> {
+                    checkIntersections(car);
+                });
+            } else {
+                car.yProperty().addListener((o) -> {
+                    checkIntersections(car);
+                });
+            }
+        }
+    }
+
+    public void checkIntersections(Car car) {
+        for (Car car2 : garage) {
+            if (car2 != car) {
+                if (Shape.intersect(car, car2).getBoundsInLocal().getWidth() != -1) {
+                    log.info("intersect");
+                    car.setIntersect(true);
+                }
+            }
+        }
+        if (Shape.intersect(car, walls).getBoundsInLocal().getWidth() != -1) {
+            log.info("intersect");
+            car.setIntersect(true);
+        }
+
+    }
+
+    public void createGarage(int longueur, int hauteur) {
+        Rectangle up = new Rectangle(0, 0, (longueur + 2) * size, size);
+        Rectangle down = new Rectangle(0, (hauteur + 1) * size, (longueur + 2) * size, size);
+        Rectangle left = new Rectangle(0, 0, size, (hauteur + 2) * size);
+        Rectangle right = new Rectangle((longueur + 1) * size, 0, size, (hauteur + 2) * size);
+
+        Shape upDown = Shape.union(up, down);
+        Shape leftRight = Shape.union(left, right);
+        Shape sides = Shape.union(upDown, leftRight);
+
+        Rectangle door = new Rectangle((longueur + 1) * size, ((hauteur / 2)) * size, size, size * 2);
+
+        walls = Shape.subtract(sides, door);
+        walls.setFill(Color.WHITE);
+        gameContext.getChildren().add(walls);
     }
 
 }
