7e71c85dc2cb50f1b279b186c209c294a3c4181d
schwab <didier.schwab@imag.fr>
2018-07-29 17:59:57 +0200

    work on issue #586 (Language names should be complete) and #587 (A flag with the language as a pictogram) : images from Wikipedia (800 px)

--- gazeplay-commons/src/main/java/net/gazeplay/commons/utils/multilinguism/NewLanguages.java
+++ gazeplay-commons/src/main/java/net/gazeplay/commons/utils/multilinguism/NewLanguages.java
@@ -1,5 +1,14 @@
 package net.gazeplay.commons.utils.multilinguism;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Languages of GazePlay
  *
@@ -31,14 +40,70 @@ package net.gazeplay.commons.utils.multilinguism;
  *
  * vnm: Vietnamese
  *
- * cf: https://www.cairn.info/revue-de-l-ofce-2012-6-page-253.htm
+ * see https://iso639-3.sil.org/code_tables/639/data/c
  *
  */
 public class NewLanguages {
 
+    private static final HashMap<String, CodeLanguagesFlags> languageMap = new HashMap<>(50);
 
+    private NewLanguages(){
+
+        put("ara", "العَرَبِيَّة", "net/gazeplay/commons/utils/multilinguism/flags/800px-Flag_of_the_Arab_League.svg.png");
+        put("deu", "Deutsch", "net/gazeplay/commons/utils/multilinguism/flags/800px-Flag_of_Germany.svg.png", "net/gazeplay/commons/utils/multilinguism/flags/800px-Flag_of_Austria.svg.png");
+        put("fra", "Français", "net/gazeplay/commons/utils/multilinguism/flags/Flag_of_La_Francophonie.svg.png", "net/gazeplay/commons/utils/multilinguism/flags/800px-Flag_of_France.svg.png");
+
+    }
+
+    /**
+     *
+     * 1st: code
+     *
+     * 2nd: language
+     *
+     * next: path to flag
+     *
+     */
+    private void put(String... args){
+
+        String code = args[0];
+        String language = args[1];
+
+        ArrayList<String> flags = new ArrayList(args.length);
+
+        for(int i = 2; i < args.length; i++){
+
+            flags.add(args[i]);
+        }
+
+        languageMap.put(code, new CodeLanguagesFlags(code, language, flags));
+
+    }
 
   //  ara, chn, deu, eng, fra, hrv, ita, jpn, por, nld, rus, spa, vnm
+}
+
+/**
+ *
+ * String code: the ISO 639 code of the language
+ *
+ * String language: the language
+ *
+ * ArrayList<String> flags corresponding to this language
+ */
+
+@Slf4j
+@NoArgsConstructor
+@AllArgsConstructor
+class CodeLanguagesFlags{
+
+    public String code;
+    public String language;
+    public ArrayList<String> flags;
+
+
+
+
 }
 
 
