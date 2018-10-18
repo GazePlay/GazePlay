6b81833e2d22c97dee5fe5abb8b686175b7d5378
schwab <didier.schwab@imag.fr>
2018-10-18 19:13:19 +0200

    Update licence

--- gazeplay-dist/src/main/bin/gazeplay.bat
+++ gazeplay-dist/src/main/bin/gazeplay.bat
@@ -2,7 +2,7 @@
 
 SET PATH=%PATH%;%LocalAppData%\TobiiStreamEngineForJava\lib\tobii\x64
 
-java -Xms256m -Xmx2g -jar ..\lib\gazeplay-${project.version}.jar
+java -Xms256m -Xmx1g -jar ..\lib\gazeplay-${project.version}.jar
 
 
 REM prints a nice "Press any key to continue . . . " message
