2ce3a9e5d77c07465886c604eea5a18aec5a1150
johzz-o2 <johana.marku@gmail.com>
2019-05-14 11:51:10 +0200

    fixing the display of the fixations sequence labels

--- gazeplay-commons/src/main/java/net/gazeplay/commons/utils/FixationSequence.java
+++ gazeplay-commons/src/main/java/net/gazeplay/commons/utils/FixationSequence.java
@@ -1,8 +1,5 @@
 package net.gazeplay.commons.utils;
 
-import com.google.common.graph.Graph;
-import com.sun.javafx.tk.FontMetrics;
-import com.sun.javafx.tk.Toolkit;
 import javafx.geometry.VPos;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.text.TextAlignment;
@@ -11,16 +8,17 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
-
 import javax.imageio.ImageIO;
-import javafx.scene.text.Font;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Vector;
+import java.lang.Math;
 
 public class FixationSequence {
 
@@ -49,26 +47,86 @@ public class FixationSequence {
         gc.setTextAlign(TextAlignment.CENTER);
         gc.setTextBaseline(VPos.CENTER);
 
-
         //draw the circles with the labels on top
         gc.setStroke(Color.RED);
-
         gc.setLineWidth(0.6);
-        int label_count = 0;
 
-        for (int j = 0; j < fixSeq.size(); j = j + 25) {
-            label_count++;
+        int label_count = 1;// for the labels of the fixation sequence
+        double alpha = -100.0; // angle between 2 lines
+
+        gc.setStroke(Color.RED);
+        int x = fixSeq.get(0).getY();
+        int y = fixSeq.get(0).getX();
+
+        int radius = 20;
+
+        gc.strokeOval(x-radius/2, y-radius/2, radius, radius);
+        gc.setFill(Color.rgb(255, 255, 0, 0.5));//yellow 50% transparency
+        gc.fillOval(x-radius/2, y-radius/2, radius, radius);
+        gc.setFill(Color.BLACK);
+        gc.fillText(Integer.toString(label_count), x, y,40);
+
+//        double[] u = new double[2];
+//        double[] v = new double[2];
+        double m1; // slope of line between points j-1 and j
+        double m2; // slope of line between points j and j+1
+
+        for (int j = 1; j < fixSeq.size()-1; j ++) {
 
             gc.setStroke(Color.RED);
-            int x = fixSeq.get(j).getY();
-            int y = fixSeq.get(j).getX();
-            int radius = 20;
-            gc.strokeOval(x-radius/2, y-radius/2, radius, radius);
-            gc.setFill(Color.rgb(255, 255, 0, 0.5));//yellow 50% transparency
-            gc.fillOval(x-radius/2, y-radius/2, radius, radius);
+            x = fixSeq.get(j).getY();
+            y = fixSeq.get(j).getX();
 
-            gc.setFill(Color.BLACK);
-            gc.fillText(Integer.toString(label_count), fixSeq.get(j).getY(), fixSeq.get(j).getX(),40);
+            if(x - fixSeq.get(j-1).getY() == 0)
+                m1 = (y - fixSeq.get(j-1).getX())/(0.0001);
+            else m1 = (y - fixSeq.get(j-1).getX())/(x - fixSeq.get(j-1).getY());
+
+            if(fixSeq.get(j+1).getY() - x == 0)
+                m2 = (fixSeq.get(j+1).getX() - y )/(0.0001);
+            else m2 = (fixSeq.get(j+1).getX() - y )/(fixSeq.get(j+1).getY() - x);
+
+            alpha = Math.atan(Math.abs((m2 - m1)/(1 + m2*m1)));
+
+
+            //ideja me cos alpha , kendi midis 2 vectoreve
+//            u[0] = ((x - fixSeq.get(j-1).getY()));
+//            u[1] = ((y - fixSeq.get(j-1).getX()));
+//
+//            v[0] = ((fixSeq.get(j+1).getY() - x));
+//            v[1] = ((fixSeq.get(j+1).getX() - y));
+//
+//            double lengthU = Math.sqrt(Math.pow(u[0],2) + Math.pow(u[1],2));
+//            double lengthV = Math.sqrt(Math.pow(v[0],2) + Math.pow(v[1],2));
+//
+//            alpha = Math.acos((u[0] * v[0] + u[1] * v[1])/(lengthU * lengthV));
+
+            // ideja me koeficient kendor
+//            if(x - fixSeq.get(j-1).getY() == 0)
+//                alpha = Math.atan(Math.PI/2);
+//            else
+//                alpha = Math.atan((y - fixSeq.get(j+1).getX())/(x - fixSeq.get(j-1).getY()));
+//
+//            if(fixSeq.get(j+1).getY() - x == 0)
+//                alpha1 = Math.atan(Math.PI/2);
+//            else
+//                alpha1 = Math.atan((fixSeq.get(j+1).getX() - y)/(fixSeq.get(j+1).getY() - x));
+
+
+            radius = 20; // radius depends on time spent on a position .
+            // if alpha < 120 degrees
+            //if(alpha < Math.acos((2*Math.PI)/3)  && alpha > Math.acos(0)/*&& alpha1 <  Math.abs(alpha-Math.acos((2*Math.PI)/3))*/ ){
+            if(alpha < Math.atan(Math.PI/2)){
+                label_count++;
+                gc.strokeOval(x-radius/2, y-radius/2, radius, radius);
+                gc.setFill(Color.rgb(255, 255, 0, 0.5));//yellow 50% transparency
+                gc.fillOval(x-radius/2, y-radius/2, radius, radius);
+                gc.setFill(Color.BLACK);
+                gc.fillText(Integer.toString(label_count), fixSeq.get(j).getY(), fixSeq.get(j).getX(),40);
+            }
+            else
+                continue;
+
+
         }
         SnapshotParameters params = new SnapshotParameters();
         params.setFill(Color.TRANSPARENT);
