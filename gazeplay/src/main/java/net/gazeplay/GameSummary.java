6311f88e833fff446594f2ac09f10d2a839f70b7
johzz-o2 <johana.marku@gmail.com>
2019-05-17 19:20:43 +0200

    .

--- gazeplay/src/main/java/net/gazeplay/GameSummary.java
+++ gazeplay/src/main/java/net/gazeplay/GameSummary.java
@@ -10,10 +10,10 @@ public class GameSummary {
     private final String nameCode;
 
     @Getter
-    private final String gameTypeIndicatorImageLocation;
+    private final String gameThumbnail;
 
     @Getter
-    private final String thumbnailLocation;
+    private final GameCategories.Category category;
 
     @Getter
     private final String backgroundMusicUrl;
@@ -21,24 +21,24 @@ public class GameSummary {
     @Getter
     private final String description;
 
-    public GameSummary(String nameCode, String gameTypeIndicatorImageLocation) {
-        this(nameCode, gameTypeIndicatorImageLocation, "data/common/images/bravo.png");
+    public GameSummary(String nameCode, String gameThumbnail) {
+        this(nameCode,gameThumbnail, GameCategories.Category.NONCATEGORIZED);
     }
 
-    public GameSummary(String nameCode, String gameTypeIndicatorImageLocation, String thumbnailLocation) {
-        this(nameCode, gameTypeIndicatorImageLocation, thumbnailLocation, null);
+    public GameSummary(String nameCode,String gameThumbnail,  GameCategories.Category category) {
+        this(nameCode,gameThumbnail, category, null);
     }
 
-    public GameSummary(String nameCode, String gameTypeIndicatorImageLocation, String thumbnailLocation,
-            final String backgroundMusicUrl) {
-        this(nameCode, gameTypeIndicatorImageLocation, thumbnailLocation, backgroundMusicUrl, null);
+    public GameSummary(String nameCode,String gameThumbnail, GameCategories.Category category,
+                       final String backgroundMusicUrl) {
+        this(nameCode,gameThumbnail, category, backgroundMusicUrl,null);
     }
 
-    public GameSummary(String nameCode, String gameTypeIndicatorImageLocation, String thumbnailLocation,
-            String backgroundMusicUrl, final String description) {
+    public GameSummary(String nameCode,String gameThumbnail, GameCategories.Category category,
+                       String backgroundMusicUrl, final String description) {
         this.nameCode = nameCode;
-        this.gameTypeIndicatorImageLocation = gameTypeIndicatorImageLocation;
-        this.thumbnailLocation = thumbnailLocation;
+        this.gameThumbnail = gameThumbnail;
+        this.category = category;
         this.backgroundMusicUrl = backgroundMusicUrl;
         this.description = description;
     }
