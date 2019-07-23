420def33c58727508f2a0330382fb9d2c95cf76a
teo blachere <tblachere@gmail.com>
2019-07-23 14:57:36 +0200

    Forgot to uncomment bit

--- gazeplay/src/main/java/net/gazeplay/games/horses/Horses.java
+++ gazeplay/src/main/java/net/gazeplay/games/horses/Horses.java
@@ -304,7 +304,7 @@ public class Horses implements GameLifeCycle {
         ArrayList<Pawn> currentPawns = pawns.get(chosenTeams.get(currentTeam));
         int nbNonMovablePawns = 0;
         for (Pawn pawn : currentPawns) {
-            if (!pawn.isOnTrack() /*&& diceOutcome == 6*/ && !startSquares.get(chosenTeams.get(currentTeam)).isOccupied()) {
+            if (!pawn.isOnTrack() && diceOutcome == 6 && !startSquares.get(chosenTeams.get(currentTeam)).isOccupied()) {
                 pawn.activate(e -> {
                     deactivatePawns();
                     pawn.spawn();
