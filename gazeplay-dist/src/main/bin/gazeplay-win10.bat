6103ae87e00b2470aa3127358907a0d536c6ac6b
milesbardon <milesbardon@gmail.com>
2019-09-14 12:37:43 +0100

    Scripts are now copied over with the name and version number correct. Changed project name to avoid overwriting gazeplay module

--- gazeplay-dist/src/main/bin/gazeplay-win10.bat
+++ gazeplay-dist/src/main/bin/gazeplay-win10.bat
@@ -2,7 +2,7 @@
 
 SET PATH=%PATH%;%LocalAppData%\TobiiStreamEngineForJava\lib\tobii\x64
 
-..\lib\jre1.8.0_221\bin\java -Xms256m -Xmx1g -jar ..\lib\gazeplay-${project.version}.jar
+java -Xms256m -Xmx1g -jar ..\lib\@NAME@-@VERSION@.jar
 
 
 REM prints a nice "Press any key to continue . . . " message
