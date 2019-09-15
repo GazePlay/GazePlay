648517e66aee1f25f034d980c720b6dd35d5a552
milesbardon <milesbardon@gmail.com>
2019-09-15 10:24:34 +0100

    Replaced JRE 1.8 with JRE 11 - Windows

--- gazeplay-dist/src/main/bin/gazeplay-win10.bat
+++ gazeplay-dist/src/main/bin/gazeplay-win10.bat
@@ -2,8 +2,7 @@
 
 SET PATH=%PATH%;%LocalAppData%\TobiiStreamEngineForJava\lib\tobii\x64
 
-java -Xms256m -Xmx1g -jar ..\lib\@NAME@-@VERSION@.jar
-
+..\lib\jre\bin\java -Xms256m -Xmx1g -jar ..\lib\@NAME@-@VERSION@.jar
 
 REM prints a nice "Press any key to continue . . . " message
 pause
