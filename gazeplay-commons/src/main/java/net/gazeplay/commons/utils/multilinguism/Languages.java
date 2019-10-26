5f81c6fdef90c286fb5bfe8caea52f5c6858c648
Yann ROBERT <yann.robert@anantaplex.fr>
2019-10-26 17:02:03 +0200

    improvements on Languages definitions

--- gazeplay-commons/src/main/java/net/gazeplay/commons/utils/multilinguism/Languages.java
+++ gazeplay-commons/src/main/java/net/gazeplay/commons/utils/multilinguism/Languages.java
@@ -1,150 +1,111 @@
 package net.gazeplay.commons.utils.multilinguism;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Languages of GazePlay
- *
+ * <p>
  * They follow ISO 639-3
- *
+ * <p>
  * alb: Albanian
- *
+ * <p>
  * ara: Arabic
- *
+ * <p>
  * chn: Chinese
- *
+ * <p>
  * deu: German
- *
+ * <p>
  * eng: English
- *
+ * <p>
  * fra: French
- *
+ * <p>
  * hrv: Croatian
- *
+ * <p>
  * ita: Italian
- *
+ * <p>
  * jpn: Japanese
- *
+ * <p>
  * pol: Polish
- *
+ * <p>
  * por: Portuguese
- *
+ * <p>
  * nld: Dutch
- *
+ * <p>
  * rus: Russian
- *
+ * <p>
  * spa: Spanish
- *
+ * <p>
  * vnm: Vietnamese
- *
+ * <p>
  * see https://iso639-3.sil.org/code_tables/639/data/c
- *
  */
 public class Languages {
 
-    private static final HashMap<String, CodeLanguageFlag> languageMap = new HashMap<>(50);
-    private static final HashMap<String, String> languageCodeMap = new HashMap<>(50);
+    private static final SortedMap<String, LanguageDetails> languageMap = Collections.unmodifiableSortedMap(createLanguagesMap());
+
     private static final String DEFAULT_CODE = "fra";
 
-    static {
-        put("alb", "Shqip", "data/common/flags/800px-Flag_of_Albania.svg.png");
-        put("ara", "العَرَبِيَّة", "data/common/flags/800px-Flag_of_the_Arab_League.svg.png");
-        put("chn", "漢語", "data/common/flags/800px-Flag_of_the_People's_Republic_of_China.svg.png");
-        put("deu", "Deutsch", "data/common/flags/800px-Flag_of_Germany.svg.png",
-                "data/common/flags/800px-Flag_of_Austria.svg.png");
-        put("eng", "English", "data/common/flags/800px-Flag_of_the_United_Kingdom.svg.png",
-                "data/common/flags/800px-Flag_of_the_United_States_(Pantone).svg.png");
-        put("ell", "ελληνικά", "data/common/flags/800px-Flag_of_Greece.svg.png");
-        put("fin", "Suomi", "data/common/flags/800px-Flag_of_Finland.svg.png");
-        put("fra", "Français", "data/common/flags/Flag_of_La_Francophonie.svg.png",
-                "data/common/flags/800px-Flag_of_France.svg.png");
-        put("hrv", "Hrvatski", "data/common/flags/800px-Flag_of_Croatia.svg.png");
-        put("ind", "Bahasa Indonesia", "data/common/flags/800px-Flag_of_Indonesia.svg.png");
-        put("ita", "Italiano", "data/common/flags/800px-Flag_of_Italy.svg.png");
-        put("jpn", "日本語", "data/common/flags/800px-Flag_of_Japan.svg.png");
-        put("pol", "polszczyzna", "data/common/flags/800px-Flag_of_Poland.svg.png");
-        put("por", "Português", "data/common/flags/800px-Flag_of_Portugal.svg.png",
-                "data/common/flags/800px-Flag_of_Brazil.svg.png");
-        put("nld", "Nederlands", "data/common/flags/800px-Flag_of_the_Netherlands.svg.png");
-        put("rus", "Pу́сский язы́к", "data/common/flags/800px-Flag_of_Russia.svg.png");
-        put("spa", "Español", "data/common/flags/800px-Flag_of_Spain.svg.png");
-        put("vnm", "Tiếng Việt", "data/common/flags/800px-Flag_of_Vietnam.svg.png");
-        put("zsm", "Bahasa Melayu", "data/common/flags/800px-Flag_of_Malaysia.svg.png");
+    private static final String flagsPath = "data/common/flags/";
+
+    private static SortedMap<String, LanguageDetails> createLanguagesMap() {
+        SortedMap<String, LanguageDetails> result = new TreeMap<>();
+        put(result, build("alb", "Shqip", false, true, "800px-Flag_of_Albania.svg.png"));
+        put(result, build("ara", "العَرَبِيَّة", false, false, "800px-Flag_of_the_Arab_League.svg.png"));
+        put(result, build("chn", "漢語", false, true, "800px-Flag_of_the_People's_Republic_of_China.svg.png"));
+        put(result, build("deu", "Deutsch", true, true, "800px-Flag_of_Germany.svg.png", "800px-Flag_of_Austria.svg.png"));
+        put(result, build("eng", "English", true, true, "800px-Flag_of_the_United_Kingdom.svg.png", "800px-Flag_of_the_United_States_(Pantone).svg.png"));
+        put(result, build("ell", "ελληνικά", false, true, "800px-Flag_of_Greece.svg.png"));
+        put(result, build("fin", "Suomi", false, true, "800px-Flag_of_Finland.svg.png"));
+        put(result, build("fra", "Français", true, true, "800px-Flag_of_France.svg.png", "Flag_of_La_Francophonie.svg.png"));
+        put(result, build("hrv", "Hrvatski", false, true, "800px-Flag_of_Croatia.svg.png"));
+        put(result, build("ind", "Bahasa Indonesia", false, true, "800px-Flag_of_Indonesia.svg.png"));
+        put(result, build("ita", "Italiano", false, true, "800px-Flag_of_Italy.svg.png"));
+        put(result, build("jpn", "日本語", false, true, "800px-Flag_of_Japan.svg.png"));
+        put(result, build("pol", "polszczyzna", false, true, "800px-Flag_of_Poland.svg.png"));
+        put(result, build("por", "Português", false, true, "800px-Flag_of_Portugal.svg.png", "800px-Flag_of_Brazil.svg.png"));
+        put(result, build("nld", "Nederlands", false, true, "800px-Flag_of_the_Netherlands.svg.png"));
+        put(result, build("rus", "Pу́сский язы́к", false, true, "800px-Flag_of_Russia.svg.png"));
+        put(result, build("spa", "Español", false, true, "800px-Flag_of_Spain.svg.png"));
+        put(result, build("vnm", "Tiếng Việt", false, true, "800px-Flag_of_Vietnam.svg.png"));
+        put(result, build("zsm", "Bahasa Melayu", false, true, "800px-Flag_of_Malaysia.svg.png"));
+        return result;
     }
 
-    /**
-     *
-     * 1st: code
-     *
-     * 2nd: language
-     *
-     * next: path to flag
-     *
-     */
-    private static void put(String... args) {
+    private static void put(SortedMap<String, LanguageDetails> result, LanguageDetails languageDetails) {
+        result.put(languageDetails.getCode(), languageDetails);
+    }
 
-        String code = args[0];
-        String language = args[1];
+    private static LanguageDetails build(String code, String language, boolean stableTranslationAvailable, boolean leftAligned, String... flags) {
+        return LanguageDetails.builder()
+            .code(code)
+            .label(language)
+            .stableTranslationAvailable(stableTranslationAvailable)
+            .leftAligned(leftAligned)
+            .flags(toFlagsLocations(flags))
+            .build();
+    }
 
-        ArrayList<String> flags = new ArrayList(args.length);
-
-        for (int i = 2; i < args.length; i++) {
-
-            flags.add(args[i]);
+    private static List<String> toFlagsLocations(String[] flags) {
+        List<String> flagsList = new ArrayList<>(flags.length);
+        for (String flag : flags) {
+            flagsList.add(flagsPath + flag);
         }
-
-        languageMap.put(code, new CodeLanguageFlag(code, language, flags));
-        languageCodeMap.put(language, code);
-
+        return Collections.unmodifiableList(flagsList);
     }
 
-    public static String getCode(String language) {
-
-        return languageCodeMap.get(language);
-    }
-
-    public static String getLanguage(String code) {
-
-        if (code == null)
+    public static LanguageDetails getLanguage(String code) {
+        if (code == null) {
             code = DEFAULT_CODE;
-
-        return languageMap.get(code).language;
+        }
+        return languageMap.get(code);
     }
 
-    public static ArrayList<String> getLanguages() {
-
-        return new ArrayList<String>(languageCodeMap.keySet());
+    public static Collection<LanguageDetails> getAllLanguageDetails() {
+        return languageMap.values();
     }
 
-    public static ArrayList<String> getCodes() {
-
-        return new ArrayList<String>(languageMap.keySet());
+    public static Collection<String> getAllCodes() {
+        return languageMap.keySet();
     }
 
-    public static ArrayList<String> getFlags(String code) {
-
-        return languageMap.get(code).flags;
-    }
-}
-
-/**
- *
- * String code: the ISO 639 code of the language
- *
- * String language: the language
- *
- * ArrayList<String> flags corresponding to this language
- */
-
-@Slf4j
-@AllArgsConstructor
-class CodeLanguageFlag {
-
-    public String code;
-    public String language;
-    public ArrayList<String> flags;
 }
