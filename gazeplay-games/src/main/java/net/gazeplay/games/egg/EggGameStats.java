23b2dd3a785ac10cdd29e86deef035f81173cc66
kuhlkrein <sebastien.riou@etu.univ-grenoble-alpes.fr>
2020-01-13 13:16:07 +0100

    Re-add egg game

---
+++ gazeplay-games/src/main/java/net/gazeplay/games/egg/EggGameStats.java
@@ -0,0 +1,15 @@
+package net.gazeplay.games.egg;
+
+import javafx.scene.Scene;
+import net.gazeplay.stats.HiddenItemsGamesStats;
+import net.gazeplay.stats.ShootGamesStats;
+
+public class EggGameStats extends ShootGamesStats {
+
+    public EggGameStats(Scene scene) {
+
+        super(scene);
+        gameName = "Egg";
+    }
+
+}
