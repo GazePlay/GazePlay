46bf0207c21d59d80c6edd09508129e3a67d5de4
kuhlkrein <sebastien.riou@etu.univ-grenoble-alpes.fr>
2020-01-16 16:34:42 +0100

    Changing name of categories logos

--- gazeplay-games-commons/src/main/java/net/gazeplay/GameCategories.java
+++ gazeplay-games-commons/src/main/java/net/gazeplay/GameCategories.java
@@ -8,11 +8,11 @@ public class GameCategories {
     private static final String categoryThumbnailsPath = "data/common/images/categoriesThumbnails/";
 
     public enum Category {
-        ACTION_REACTION("ActionReaction games", categoryThumbnailsPath + "searching-magnifying-glass.png"),
+        ACTION_REACTION("ActionReaction games", categoryThumbnailsPath + "action_reaction.png"),
         SELECTION("Selection games", categoryThumbnailsPath + "target.png"),
         MEMORIZATION("Memorization games", categoryThumbnailsPath + "male-brain.png"),
-        LOGIC_MATHS("Logic and Maths games", categoryThumbnailsPath + "creativity-icon.png"),
-        LITERACY("Literacy games", categoryThumbnailsPath + "logic_gear.png"),
+        LOGIC_MATHS("Logic and Maths games", categoryThumbnailsPath + "logic_icon.png"),
+        LITERACY("Literacy games", categoryThumbnailsPath + "literacy_icon.png"),
         MULTIMEDIA("Multimedia", categoryThumbnailsPath + "utility-icon.png");
 
         @NonNull
