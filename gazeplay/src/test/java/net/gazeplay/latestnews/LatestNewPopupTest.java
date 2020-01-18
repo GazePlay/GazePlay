7edc8130e8c83bab2ed1b3de444de0beef445ae8
milesbardon <milesbardon@gmail.com>
2020-01-18 21:00:00 +0000

    Dynamically generating test MF file

--- gazeplay/src/test/java/net/gazeplay/latestnews/LatestNewPopupTest.java
+++ gazeplay/src/test/java/net/gazeplay/latestnews/LatestNewPopupTest.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.beans.property.SimpleLongProperty;
 import net.gazeplay.commons.configuration.Configuration;
 import net.gazeplay.commons.ui.Translator;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,14 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.testfx.framework.junit5.ApplicationExtension;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -24,7 +33,7 @@ import static org.mockito.MockitoAnnotations.initMocks;
 class LatestNewPopupTest {
 
     @Mock
-    Translator mockTanslator;
+    Translator mockTranslator;
     @Mock
     private Configuration mockConfig;
 
@@ -33,16 +42,24 @@ class LatestNewPopupTest {
         initMocks(this);
     }
 
+    void createMockManifest(List<String> lines) throws IOException {
+        new File("build/resources/test/META-INF").mkdir();
+        Path file = Paths.get("build/resources/test/META-INF/MANIFEST.MF");
+        Files.write(file, lines, StandardCharsets.UTF_8);
+    }
+
     @Test
-    void shouldCreateDocumentURI() {
+    void shouldCreateDocumentURI() throws IOException {
+        List<String> lines = Arrays.asList("Implementation-Title: gazeplay", "Implementation-Version: 1.7");
+        createMockManifest(lines);
         long mockLastTime = System.currentTimeMillis() - TimeUnit.DAYS.toMillis(1);
 
         when(mockConfig.getLatestNewsPopupShownTime()).thenReturn(new SimpleLongProperty(mockLastTime - 100));
         when(mockConfig.getLanguage()).thenReturn("eng");
-        when(mockTanslator.translate(ArgumentMatchers.<String>any())).thenReturn("some translation");
+        when(mockTranslator.translate(ArgumentMatchers.<String>any())).thenReturn("some translation");
 
         Platform.runLater(() -> {
-            LatestNewPopup latestNewPopup = new LatestNewPopup(mockConfig, mockTanslator);
+            LatestNewPopup latestNewPopup = new LatestNewPopup(mockConfig, mockTranslator);
             assertEquals("gazeplay-1-7-eng", latestNewPopup.createDocumentUri());
         });
     }
