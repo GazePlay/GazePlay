0a2b03f3dfc5af0a60b976c53fca006ce8802ce5
Yann ROBERT <yann.robert@anantaplex.fr>
2020-01-30 21:23:47 +0100

    use javafx.stage.Screen instead of com.sun.glass.ui.Screen

--- gazeplay-commons/src/main/java/net/gazeplay/commons/utils/ScreenDimensionSupplier.java
+++ gazeplay-commons/src/main/java/net/gazeplay/commons/utils/ScreenDimensionSupplier.java
@@ -1,14 +1,16 @@
 package net.gazeplay.commons.utils;
 
-import com.sun.glass.ui.Screen;
 import javafx.geometry.Dimension2D;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 
 import java.util.function.Supplier;
 
 public class ScreenDimensionSupplier implements Supplier<Dimension2D> {
     @Override
     public Dimension2D get() {
-        Screen mainScreen = Screen.getMainScreen();
-        return new Dimension2D(mainScreen.getWidth(), mainScreen.getHeight());
+        Screen primaryScreen = Screen.getPrimary();
+        Rectangle2D bounds = primaryScreen.getBounds();
+        return new Dimension2D(bounds.getWidth(), bounds.getHeight());
     }
 }
