09f7f68994ed4a2b8819e887e7d882b6d9b21f72
Yann ROBERT <yann.robert@anantaplex.fr>
2020-02-14 01:29:28 +0100

    let lombok generate the constructor

--- gazeplay-commons/src/main/java/net/gazeplay/commons/configuration/observableproperties/EnumUnmarshaller.java
+++ gazeplay-commons/src/main/java/net/gazeplay/commons/configuration/observableproperties/EnumUnmarshaller.java
@@ -1,18 +1,16 @@
 package net.gazeplay.commons.configuration.observableproperties;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.function.Function;
 
+@RequiredArgsConstructor
 public class EnumUnmarshaller<T extends Enum<T>> implements Function<String, T> {
 
-	private final Class<T> enumClass;
+    private final Class<T> enumClass;
 
-	public EnumUnmarshaller(final Class<T> enumClass) {
-		super();
-		this.enumClass = enumClass;
-	}
-
-	@Override
-	public T apply(final String name) {
-		return Enum.valueOf(enumClass, name);
-	}
+    @Override
+    public T apply(final String name) {
+        return Enum.valueOf(enumClass, name);
+    }
 }
