30035c6de85d1091aa1eacf836fcb92238aa6994
kuhlkrein <sebastien.riou@etu.univ-grenoble-alpes.fr>
2020-02-26 16:19:44 +0100

    begining of implementation with gridpane

--- gazeplay/src/main/java/net/gazeplay/ui/scenes/configuration/ConfigurationContext.java
+++ gazeplay/src/main/java/net/gazeplay/ui/scenes/configuration/ConfigurationContext.java
@@ -10,20 +10,22 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.TextAlignment;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
+import javafx.stage.*;
 import javafx.util.StringConverter;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -37,12 +39,11 @@ import net.gazeplay.commons.ui.I18NText;
 import net.gazeplay.commons.ui.Translator;
 import net.gazeplay.commons.utils.ControlPanelConfigurator;
 import net.gazeplay.commons.utils.HomeButton;
-import net.gazeplay.commons.utils.games.BackgroundMusicManager;
-import net.gazeplay.commons.utils.games.GazePlayDirectories;
-import net.gazeplay.commons.utils.games.Utils;
+import net.gazeplay.commons.utils.games.*;
 import net.gazeplay.commons.utils.multilinguism.LanguageDetails;
 import net.gazeplay.commons.utils.multilinguism.Languages;
 import net.gazeplay.components.CssUtil;
+import net.gazeplay.components.Portrait;
 import net.gazeplay.ui.GraphicalContext;
 import net.gazeplay.ui.scenes.gamemenu.GameButtonOrientation;
 
@@ -55,6 +56,7 @@ import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
@@ -259,7 +261,7 @@ public class ConfigurationContext extends GraphicalContext<BorderPane> {
         {
             I18NText label = new I18NText(translator, "FileDir", COLON);
 
-            Node input = buildDirectoryChooser(config, configurationContext, translator, DirectoryType.FILE);
+            Node input = buildImageChooser();
 
             addToGrid(grid, currentFormRow, label, input);
         }
@@ -528,6 +530,117 @@ public class ConfigurationContext extends GraphicalContext<BorderPane> {
 
     enum DirectoryType {
         FILE, WHERE_IS_IT, MUSIC, VIDEO
+    }
+
+    Node buildImageChooser(){
+
+        final Button selectButton = new Button( "select");
+        Stage dialog = buildCustomColorDialog();
+
+        selectButton.setOnAction(e -> {
+            dialog.show();
+            dialog.sizeToScene();
+        });
+
+        return  selectButton;
+    }
+
+    private Stage buildCustomColorDialog() {
+        final Stage primaryStage = getGazePlay().getPrimaryStage();
+        final Stage dialog = new Stage();
+        dialog.initOwner(primaryStage);
+        dialog.initModality(Modality.WINDOW_MODAL);
+        dialog.initStyle(StageStyle.UTILITY);
+        dialog.setOnCloseRequest(
+            windowEvent -> primaryStage.getScene().getRoot().setEffect(null));
+        dialog.setTitle("customColorDialogTitle");
+        dialog.setAlwaysOnTop(true);
+
+        Pane imageSelector = createImageSelectorPane();
+        imageSelector.minWidth(500);
+        imageSelector.minHeight(500);
+
+       final Scene scene = new Scene(imageSelector, 500,500, Color.TRANSPARENT);
+
+        imageSelector.minWidthProperty().bind(scene.widthProperty());
+        imageSelector.minHeightProperty().bind(scene.heightProperty());
+        imageSelector.prefWidthProperty().bind(scene.widthProperty());
+        imageSelector.prefHeightProperty().bind(scene.heightProperty());
+
+        dialog.setScene(scene);
+
+        return dialog;
+    }
+
+    DropShadow createOngletShadow(){
+        DropShadow dropShadowRight = new DropShadow();
+        dropShadowRight.setRadius(10);
+        dropShadowRight.setOffsetX(0);
+        dropShadowRight.setOffsetY(0);
+        dropShadowRight.setColor(Color.color(0, 0, 0,0.5));
+        return dropShadowRight;
+    }
+
+    void updatGridPane(GridPane gp) {
+        ImageLibrary lib = ImageUtils.createImageLibrary(Utils.getImagesSubDirectory("portraits"));
+        Set<Image> set = lib.pickMultipleRandomDistinctImages(10);
+        gp.setBackground(new Background(new BackgroundFill(Color.YELLOW,CornerRadii.EMPTY,null)));
+        int index = 0;
+        for(Image i : set){
+            log.info("the index of this image is {}",index);
+            ImageView iv = new ImageView(i);
+            iv.setFitWidth(90);
+            iv.setFitHeight(90);
+            GridPane.setRowIndex( iv,index / 5);
+            GridPane.setColumnIndex( iv,index % 5);
+            gp.getChildren().add(iv);
+            index++;
+        }
+    }
+
+    Pane createImageSelectorPane(){
+        Pane p = new Pane();
+
+        Group[] group = new Group[3];
+        BorderPane[] background = new BorderPane[3];
+        Rectangle[] onglets = new Rectangle[3];
+
+        DropShadow dropShadowOnglet = createOngletShadow();
+
+        for (int i = 0; i<3 ; i++){
+            group[i] = new Group();
+
+            background[i] = new BorderPane();
+            background[i].setLayoutY(100);
+            background[i].setLayoutX(0);
+            background[i].minWidthProperty().bind(p.widthProperty());
+            background[i].minHeightProperty().bind(p.heightProperty().subtract(100));
+            background[i].setBackground(new Background(new BackgroundFill(Color.DARKGRAY,CornerRadii.EMPTY,null)));
+
+            GridPane gp = new GridPane();
+            updatGridPane(gp);
+
+            background[i].setCenter(gp);
+
+            onglets[i] = new Rectangle();
+            onglets[i].setHeight(100);
+            onglets[i].widthProperty().bind(background[i].widthProperty().divide(3));
+            onglets[i].setY(0);
+            onglets[i].setFill(Color.DARKGRAY);
+            int index = i;
+            onglets[i].setOnMouseClicked( e-> {
+                group[index].toFront();
+            });
+
+            group[i].getChildren().addAll(background[i],onglets[i]);
+            group[i].setEffect(dropShadowOnglet);
+        }
+        onglets[0].setX(0);
+        onglets[1].xProperty().bind(onglets[0].widthProperty());
+        onglets[2].xProperty().bind(onglets[0].widthProperty().add(onglets[1].widthProperty()));
+
+        p.getChildren().addAll(group);
+        return p;
     }
 
     Node buildDirectoryChooser(
