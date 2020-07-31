45532a3e6e2ec1e405c18e7f80322eb36ebb39f2
Tohaker <milesbardon@gmail.com>
2020-07-31 22:57:27 +0100

    Adding username menu to menubar, and renaming class to better represent it

--- gazeplay-commons/src/main/java/net/gazeplay/commons/utils/games/MenuUtils.java
+++ gazeplay-commons/src/main/java/net/gazeplay/commons/utils/games/MenuUtils.java
@@ -4,6 +4,7 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import net.gazeplay.commons.VersionInfo;
+import net.gazeplay.commons.configuration.ActiveConfigurationContext;
 import org.apache.commons.io.IOUtils;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
@@ -12,9 +13,9 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
-public class LicenseUtils {
+public class MenuUtils {
 
-    public static MenuBar buildLicenceMenuBar() {
+    public static MenuBar buildMenuBar() {
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         String licenseFileAsString = loadLicenseFileAsString(classLoader);
 
@@ -23,11 +24,14 @@ public class LicenseUtils {
 
         MenuItem licenseMenuItem = new MenuItem(licenseFileAsString);
 
-        Menu menu = new Menu(versionNumber.map(value -> " GazePlay " + value).orElse("GazePlay unreleased version"));
-        menu.getItems().add(licenseMenuItem);
+        Menu versionMenu = new Menu(versionNumber.map(value -> "GazePlay " + value).orElse("GazePlay unreleased version"));
+        versionMenu.getItems().add(licenseMenuItem);
+
+        String username = ActiveConfigurationContext.getInstance().getUserName();
+        Menu profileMenu = new Menu("Current Profile: " + (username.isEmpty() ? "Default" : username));
 
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(menu);
+        menuBar.getMenus().addAll(versionMenu, profileMenu);
         menuBar.setPrefHeight(40);
         menuBar.setPrefWidth(80);
 
