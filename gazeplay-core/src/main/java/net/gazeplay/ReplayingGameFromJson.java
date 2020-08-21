9946849ba8867dda5f12145db3ec6a8fc5e1f953
Dalabayev <dalabayev.e@gmail.com>
2020-08-21 23:43:12 +0600

    some improvements in ReplayingGameFromJson

--- gazeplay-core/src/main/java/net/gazeplay/ReplayingGameFromJson.java
+++ gazeplay-core/src/main/java/net/gazeplay/ReplayingGameFromJson.java
@@ -138,23 +138,17 @@ public class ReplayingGameFromJson {
         }
         IGameLauncher gameLauncher = selectedGameSpec.getGameLauncher();
         final Scene scene = gazePlay.getPrimaryScene();
-        //final Stats stats = gameLauncher.createNewStats(scene);
         final Stats statsSaved = gameLauncher.createSavedStats(scene, nbGoalsReached, nbGoalsToReach, nbUnCountedGoalsReached, fixationSequence, lifeCycle, roundsDurationReport, savedStatsInfo);
         GameLifeCycle currentGame = gameLauncher.replayGame(gameContext, gameVariant, statsSaved, currentGameSeed);
-        //GameLifeCycle currentGame = gameLauncher.createNewGame(gameContext, gameVariant, stats);
         gameContext.createControlPanel(gazePlay, statsSaved, currentGame, "replay");
         gameContext.createQuitShortcut(gazePlay, statsSaved, currentGame);
         currentGame.launch();
-
-        /*var game = gameContext.getChildren().get(gameContext.getChildren().indexOf(currentGame));
-        gameContext.getRoot().getChildren().get(gameContext.getRoot().getChildren().indexOf(swingNode)).toFront();*/
 
         final Dimension2D screenDimension = gameContext.getCurrentScreenDimensionSupplier().get();
         //Drawing in canvas
         final javafx.scene.canvas.Canvas canvas = new Canvas(screenDimension.getWidth(), screenDimension.getHeight());
         gameContext.getChildren().add(canvas);
-        //drawFixationLines(canvas, coordinatesAndTimeStamp);
-        Stats statsSaved2 = gameLauncher.createSavedStats(scene, nbGoalsReached, nbGoalsToReach, nbUnCountedGoalsReached, fixationSequence, lifeCycle, roundsDurationReport, savedStatsInfo);
+
         Service<Void> loadingService = new Service<Void>(){
             @Override
             protected Task<Void> createTask() {
@@ -170,49 +164,24 @@ public class ReplayingGameFromJson {
 
         loadingService.setOnSucceeded( e -> {
             loadingService.reset();
-            gameContext.exitGame(statsSaved2, gazePlay, currentGame, "replay");
+            gameContext.exitGame(statsSaved, gazePlay, currentGame, "replay");
         });
 
         loadingService.setOnFailed(e -> {
             loadingService.restart();
         });
 
-        // cancelled?
         loadingService.setOnCancelled(e -> {
         });
 
         if (!loadingService.isRunning()) {
             loadingService.start();
         }
-
-
-
-        //Drawing in swingNode
-        /*BasicAnim anim  = new BasicAnim(coordinatesAndTimeStamp);
-        final SwingNode swingNode = new SwingNode();
-        swingNode.setContent(anim);
-        swingNode.resize(screenDimension.getWidth(), screenDimension.getHeight());
-        gameContext.getRoot().getChildren().add(swingNode);
-        gameContext.getRoot().getChildren().get(gameContext.getRoot().getChildren().indexOf(swingNode)).toFront();
-        anim.startAnim();*/
-
     }
 
     private static void drawFixationLines(Canvas canvas, JsonArray coordinatesAndTimeStamp) {
         final GraphicsContext graphics = canvas.getGraphicsContext2D();
-
-        /*JsonObject  startingCoordinates = (JsonObject) coordinatesAndTimeStamp.get(0);
-        x0 = Integer.parseInt(String.valueOf(startingCoordinates.get("X")));
-        y0 = Integer.parseInt(String.valueOf(startingCoordinates.get("Y")));*/
-        //graphics.setStroke(javafx.scene.paint.Color.rgb(255, 157, 6, 1));
-        //graphics.setLineWidth(4);
-
         for (JsonElement coordinateAndTimeStamp : coordinatesAndTimeStamp) {
-            /*if (!first) {
-                x0 = nextX;
-                y0 = nextY;
-                prevTime = nextTime;
-            }*/
             prevTime = nextTime;
             JsonObject coordinateAndTimeObj = coordinateAndTimeStamp.getAsJsonObject();
             nextX = Integer.parseInt(coordinateAndTimeObj.get("X").getAsString());
@@ -220,10 +189,6 @@ public class ReplayingGameFromJson {
             nextTime = Integer.parseInt(coordinateAndTimeObj.get("time").getAsString());
             delay = nextTime - prevTime;
             paint(graphics, canvas);
-            /*if (!first)
-                repaint(graphics, canvas);
-            else
-                first = false;*/
             try {
                 Thread.sleep(delay);
             } catch (InterruptedException e) {
@@ -231,20 +196,13 @@ public class ReplayingGameFromJson {
         }
     }
 
-    public static void repaint(GraphicsContext graphics, Canvas canvas) {
-        paint(graphics, canvas);
-    }
-
     public static void paint(GraphicsContext graphics, Canvas canvas) {
-
-        //g.strokeLine(x0, y0, nextX, nextY);
         graphics.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         graphics.setStroke(javafx.scene.paint.Color.RED);
         graphics.strokeOval(nextX, nextY, 50, 50);
         graphics.setFill(javafx.scene.paint.Color.rgb(255, 255, 0, 0.5));
         graphics.fillOval(nextX, nextY, 50, 50);
-        //graphics.lineTo(nextX, nextY);
-        //graphics.stroke();
+
         Point2D point = new Point2D(nextX, nextY);
         gameContext.getGazeDeviceManager().onSavedMovementsUpdate(point);
     }
@@ -277,92 +235,4 @@ class JsonFile {
     private RoundsDurationReport roundsDurationReport;
     @Getter
     private LinkedList<FixationPoint> fixationSequence;
-
-/*    public double getGameSeed() {
-        return gameSeed;
-    }
-
-    public String getGameName() {
-        return gameName;
-    }
-
-    public String getGameVariantClass() {
-        return gameVariantClass;
-    }
-
-    public String getGameVariant() {
-        return gameVariant;
-    }
-
-    public double getGameStartedTime() {
-        return gameStartedTime;
-    }
-
-    public String getScreenAspectRatio() {
-        return screenAspectRatio;
-    }
-
-    public JsonArray getCoordinatesAndTimeStamp() {return coordinatesAndTimeStamp;}*/
-
 }
-
-class BasicAnim extends JPanel {
-    private static JsonArray CoordinatesAndTimeStamp;
-    // location
-    private static int x0, y0;
-    int nextX, nextY, nextTime, prevTime;
-    // refresh rate
-    int delay = 0;
-    Graphics2D graph;
-    boolean first = true;
-
-    public BasicAnim(JsonArray coordinatesAndTimeStamp) {
-        CoordinatesAndTimeStamp = coordinatesAndTimeStamp;
-        JsonObject  startingCoordinates = (JsonObject) CoordinatesAndTimeStamp.get(0);
-        x0 = Integer.parseInt(String.valueOf(startingCoordinates.get("X")));
-        y0 = Integer.parseInt(String.valueOf(startingCoordinates.get("Y")));
-    }
-
-    public void update(Graphics g) {
-        paint(g);
-    }
-
-    public void paint(Graphics g) {
-        //super.paintComponent(g);
-        //graph = (Graphics2D)g;
-        g.setColor(Color.red);
-        //graph.drawOval(x, y, dim, dim);
-
-        g.drawLine(x0, y0, nextX, nextY);
-    }
-
-
-    public void startAnim() {
-        for (JsonElement pa : CoordinatesAndTimeStamp) {
-            if (!first) {
-                x0 = nextX;
-                y0 = nextY;
-                prevTime = nextTime;
-            }
-            JsonObject paymentObj = pa.getAsJsonObject();
-            nextX = Integer.parseInt(paymentObj.get("X").getAsString());
-            nextY = Integer.parseInt(paymentObj.get("Y").getAsString());
-            nextTime = Integer.parseInt(paymentObj.get("time").getAsString());
-            delay = nextTime - prevTime;
-            //System.out.println("x0: " + x0 + ". y0: " + y0 + ". nextX: " + nextX + ". nextY: " + nextY);
-            if (!first)
-                repaint();
-            else
-                first = false;
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-            }
-        }
-    }
-
-    public Dimension getPreferredSize(){
-        return new Dimension(1920, 1200);
-    }
-}
-
