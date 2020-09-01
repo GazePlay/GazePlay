a1a60e8b90c9715e5029183b15026b054a2475bb
kuhlkrein <sebastien.riou@univ-grenoble-alpes.fr>
2020-09-01 11:03:49 +0200

    remove blank space + reformat

--- gazeplay-api/src/main/java/com/gazeplay/application/LauncherApplication.java
+++ gazeplay-api/src/main/java/com/gazeplay/application/LauncherApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.gazeplay.controller"})
+@ComponentScan(basePackages = {"com.gazeplay.controller"})
 public class LauncherApplication {
 
     public static void main(String[] args) {
