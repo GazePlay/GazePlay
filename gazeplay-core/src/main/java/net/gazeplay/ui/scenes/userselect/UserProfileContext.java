b138db8d1eb99fc1b2f8c11a5d245219c1264c16
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2021-05-31 09:44:36 +0200

    clean

--- gazeplay-core/src/main/java/net/gazeplay/ui/scenes/userselect/UserProfileContext.java
+++ gazeplay-core/src/main/java/net/gazeplay/ui/scenes/userselect/UserProfileContext.java
@@ -595,8 +595,6 @@ public class UserProfileContext extends GraphicalContext<BorderPane> {
             primaryStage.getHeight() / 10,
             primaryStage.getWidth() / 10
         );
-        //choicePane.getChildren().add(okButton);
-
         final ProgressButton Bok = new ProgressButton();
 
         Pok.getChildren().addAll(okButton, Bok);
