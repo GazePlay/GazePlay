d98292eb38fddcf2b03346781825e61163d1fbbc
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2021-07-05 16:26:26 +0200

    less warning

--- gazeplay-games/src/main/java/net/gazeplay/games/follow/FollowGameLauncher.java
+++ gazeplay-games/src/main/java/net/gazeplay/games/follow/FollowGameLauncher.java
@@ -5,13 +5,11 @@ import net.gazeplay.GameLifeCycle;
 import net.gazeplay.IGameContext;
 import net.gazeplay.IGameLauncher;
 import net.gazeplay.commons.gamevariants.EnumGameVariant;
-import net.gazeplay.commons.gamevariants.IGameVariant;
 import net.gazeplay.commons.utils.stats.Stats;
 import net.gazeplay.commons.utils.FixationPoint;
 import net.gazeplay.commons.utils.stats.LifeCycle;
 import net.gazeplay.commons.utils.stats.RoundsDurationReport;
 import net.gazeplay.commons.utils.stats.SavedStatsInfo;
-import net.gazeplay.games.cakes.CakeGameVariant;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
