6fca9f186a8081e9a9cb8645e56f18fcf725d8e7
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2021-07-19 09:05:50 +0200

    thumbnail

--- gazeplay-games/src/main/java/net/gazeplay/games/samecolor/SameColorGameSpecSource.java
+++ gazeplay-games/src/main/java/net/gazeplay/games/samecolor/SameColorGameSpecSource.java
@@ -9,7 +9,7 @@ public class SameColorGameSpecSource implements GameSpecSource {
     @Override
     public GameSpec getGameSpec() {
         return new GameSpec(
-            GameSummary.builder().nameCode("SameColor").gameThumbnail("data/Thumbnails/biboulejump.png")
+            GameSummary.builder().nameCode("SameColor").gameThumbnail("data/Thumbnails/SameColor.png")
                 .category(GameCategories.Category.SELECTION)
                 .build(),
             new SameColorGameVariantGenerator(), new SameColorGameLauncher());
