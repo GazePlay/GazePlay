5c4ff39fdc0ec7c5ea2a3ce93942c6987fa46cee
verafili <18veraf99@gmail.com>
2021-07-22 14:05:55 +0200

    [wip] new dynamic variants

--- gazeplay-games/src/main/java/net/gazeplay/games/dottodot/DotToDotGameVariant.java
+++ gazeplay-games/src/main/java/net/gazeplay/games/dottodot/DotToDotGameVariant.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum DotToDotGameVariant {
     NUMBERS("ConnectNumbers"),
-    ORDER("ConnectOrder");
+    ORDER("ConnectOrder"),
+    NUMBER_DYNAMIC("ConnectNumbersDynamic"),
+    ORDER_DYNAMIC("ConnectOrderDynamic");
 
     @Getter
     private final String label;
