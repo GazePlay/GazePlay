22c29195cc1bfcffa0b8c3b164df8fcc716f82e0
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2021-09-01 15:32:45 +0200

    move/win/find function + Circle player (maybe chante into Rectangle later)

--- gazeplay-games/src/main/java/net/gazeplay/games/ladder/Ladder.java
+++ gazeplay-games/src/main/java/net/gazeplay/games/ladder/Ladder.java
@@ -1,14 +1,17 @@
 package net.gazeplay.games.ladder;
 
+import javafx.animation.PauseTransition;
 import javafx.geometry.Dimension2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import net.gazeplay.GameLifeCycle;
 import net.gazeplay.IGameContext;
 import net.gazeplay.commons.random.ReplayablePseudoRandom;
 import net.gazeplay.commons.utils.stats.Stats;
+import net.gazeplay.components.ProgressButton;
 
 import java.util.ArrayList;
 
@@ -31,6 +34,12 @@ public class Ladder implements GameLifeCycle {
     private double spaceh;
 
     private Circle player;
+    private double px;
+    private double py;
+
+    private double n;
+
+    private final ArrayList<ProgressButton> progressButtons;
 
     Ladder(IGameContext gameContext, Stats stats){
         this.gameContext = gameContext;
@@ -39,6 +48,7 @@ public class Ladder implements GameLifeCycle {
         dimension2D = gameContext.getGamePanelDimensionProvider().getDimension2D();
         steps = new ArrayList<>();
         fall = new ArrayList<>();
+        progressButtons = new ArrayList<>();
     }
 
     Ladder(IGameContext gameContext, Stats stats, double gameSeed){
@@ -48,6 +58,7 @@ public class Ladder implements GameLifeCycle {
         dimension2D = gameContext.getGamePanelDimensionProvider().getDimension2D();
         steps = new ArrayList<>();
         fall = new ArrayList<>();
+        progressButtons = new ArrayList<>();
     }
 
     @Override
@@ -59,6 +70,8 @@ public class Ladder implements GameLifeCycle {
         spacew = (dimension2D.getWidth()-2*ecartw)/4;
         ecarth = dimension2D.getHeight()*0.1;
         spaceh = (dimension2D.getHeight()-2*ecarth)/size;
+
+        player = new Circle(0,0,dimension2D.getHeight()/10);
 
         background();
         creation();
@@ -117,5 +130,70 @@ public class Ladder implements GameLifeCycle {
 
     private void button(){
 
+    }
+
+    private void move(Step step, boolean start){
+        steps.remove(step);
+        fall.remove(step);
+        n = 0;
+        //player.setCenterY(ecartw + px*spacew);
+        //player.setCenterY(ecarth +py*spaceh);
+        PauseTransition wait = new PauseTransition(Duration.millis(10));
+        wait.setOnFinished(event -> {
+            if (n<100){
+                n++;
+                if (start){
+                    player.setCenterX(ecartw + (double)(step.x1- step.x2)/100 * n*spacew);
+                    player.setCenterY(ecarth + (double)(step.y1- step.y2)/100 * n*spaceh);
+                } else {
+                    player.setCenterX(ecartw + (double)(step.x1- step.x2)/100 * (100-n)*spacew);
+                    player.setCenterY(ecarth + (double)(step.y1- step.y2)/100 * (100-n)*spaceh);
+                }
+                wait.play();
+            } else {
+                if (start){
+                    find(step.x2, step.y2);
+                } else {
+                    find (step.x1, step.y1);
+                }
+            }
+        });
+    }
+
+    private void find(int x, int y){
+        boolean search = true;
+        for (Step step : steps){
+            if (search && step.x1==x && step.y1==y){
+                search = false;
+                move(step, true);
+            }
+            else if (search && step.x2==x && step.y2==y){
+                search = false;
+                move(step, false);
+            }
+        }
+        for (Step step : fall){
+            if (search && step.x1==x && step.y1==y){
+                search = false;
+                move(step, true);
+            }
+        }
+        if (search){
+            win();
+        }
+    }
+
+    private void win() {
+        stats.stop();
+
+        gameContext.updateScore(stats, this);
+
+        gameContext.playWinTransition(500, actionEvent -> {
+            gameContext.getGazeDeviceManager().clear();
+
+            gameContext.clear();
+
+            gameContext.showRoundStats(stats, this);
+        });
     }
 }
