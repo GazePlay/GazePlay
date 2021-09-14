9b120c891565e1cb0379ba83f9b71e17a93946e3
kuhlkrein <sebastien.riou@univ-grenoble-alpes.fr>
2021-09-14 15:23:38 +0200

    using Translate transition animation instead of wait with steps

--- gazeplay-games/src/main/java/net/gazeplay/games/ladder/Ladder.java
+++ gazeplay-games/src/main/java/net/gazeplay/games/ladder/Ladder.java
@@ -1,6 +1,7 @@
 package net.gazeplay.games.ladder;
 
-import javafx.animation.PauseTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.geometry.Dimension2D;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
@@ -48,7 +49,7 @@ public class Ladder implements GameLifeCycle {
 
     private final ImageLibrary imageLibrary;
 
-    Ladder(IGameContext gameContext, Stats stats){
+    Ladder(IGameContext gameContext, Stats stats) {
         this.gameContext = gameContext;
         this.stats = stats;
         random = new ReplayablePseudoRandom();
@@ -60,7 +61,7 @@ public class Ladder implements GameLifeCycle {
         imageLibrary = Portrait.createImageLibrary(random);
     }
 
-    Ladder(IGameContext gameContext, Stats stats, double gameSeed){
+    Ladder(IGameContext gameContext, Stats stats, double gameSeed) {
         this.gameContext = gameContext;
         this.stats = stats;
         random = new ReplayablePseudoRandom(gameSeed);
@@ -73,7 +74,7 @@ public class Ladder implements GameLifeCycle {
     }
 
     @Override
-    public void launch(){
+    public void launch() {
         steps.clear();
         fall.clear();
         progressButtons.clear();
@@ -81,17 +82,17 @@ public class Ladder implements GameLifeCycle {
         size = 10;
         cross = 3;
 
-        ecartw = dimension2D.getWidth()*0.15;
-        spacew = (dimension2D.getWidth()-2*ecartw)/4;
-        ecarth = dimension2D.getHeight()*0.2;
-        spaceh = (dimension2D.getHeight()-2*ecarth)/size;
-        radius = ecarth/3;
+        ecartw = dimension2D.getWidth() * 0.15;
+        spacew = (dimension2D.getWidth() - 2 * ecartw) / 4;
+        ecarth = dimension2D.getHeight() * 0.2;
+        spaceh = (dimension2D.getHeight() - 2 * ecarth) / size;
+        radius = ecarth / 3;
 
         background();
         creation();
         button();
 
-        player = new Rectangle(0,0,dimension2D.getHeight()/10, dimension2D.getHeight()/10);
+        player = new Rectangle(0, 0, dimension2D.getHeight() / 10, dimension2D.getHeight() / 10);
         player.setFill(new ImagePattern(new Image("data/ladder/Biboule.png")));
 
         stats.notifyNewRoundReady();
@@ -100,23 +101,23 @@ public class Ladder implements GameLifeCycle {
     }
 
     @Override
-    public void dispose(){
+    public void dispose() {
 
     }
 
-    private void background(){
-        Rectangle back = new Rectangle(0,0,dimension2D.getWidth(), dimension2D.getHeight());
+    private void background() {
+        Rectangle back = new Rectangle(0, 0, dimension2D.getWidth(), dimension2D.getHeight());
         back.setFill(Color.WHITE);
         gameContext.getChildren().add(back);
     }
 
-    private void creation(){
+    private void creation() {
         Step s;
-        for (int i=0; i<5; i++){
-            for (int j=0; j<size; j++){
-                s = new Step(i,j,i,j+1);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < size; j++) {
+                s = new Step(i, j, i, j + 1);
                 fall.add(s);
-                if (j==0){
+                if (j == 0) {
                     start[i] = s;
                 }
             }
@@ -124,58 +125,58 @@ public class Ladder implements GameLifeCycle {
 
         int y1;
         int y2;
-        int[] old = new int[] {-1, -1, -1};
-        for (int i=0; i<4; i++){
-            for (int j=0; j<cross; j++){
-                y1 = j*3 + random.nextInt(3) + 1;
-                while (y1==old[j]){
-                    y1 = j*3 + random.nextInt(3) + 1;
+        int[] old = new int[]{-1, -1, -1};
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < cross; j++) {
+                y1 = j * 3 + random.nextInt(3) + 1;
+                while (y1 == old[j]) {
+                    y1 = j * 3 + random.nextInt(3) + 1;
                 }
-                y2 = j*3 + random.nextInt(3) + 1;
+                y2 = j * 3 + random.nextInt(3) + 1;
                 old[j] = y2;
-                steps.add(new Step(i, y1, i+1,y2));
+                steps.add(new Step(i, y1, i + 1, y2));
             }
         }
 
-        for (Step step : fall){
+        for (Step step : fall) {
             screencreation(step);
         }
-        for (Step step : steps){
+        for (Step step : steps) {
             screencreation(step);
         }
 
         Image end;
         Rectangle r;
-        double place = dimension2D.getHeight()/8;
-        for (int i=0; i<5; i++){
+        double place = dimension2D.getHeight() / 8;
+        for (int i = 0; i < 5; i++) {
             end = imageLibrary.pickRandomImage();
-            r = new Rectangle(ecartw + i*spacew - place/2,4*dimension2D.getHeight()/5,place,place);
+            r = new Rectangle(ecartw + i * spacew - place / 2, 4 * dimension2D.getHeight() / 5, place, place);
             r.setFill(new ImagePattern(end));
             gameContext.getChildren().add(r);
         }
     }
 
-    private void screencreation(Step step){
-        Line line = new Line(ecartw+spacew*step.x1,ecarth+spaceh*step.y1,ecartw+spacew*step.x2,ecarth+spaceh*step.y2);
+    private void screencreation(Step step) {
+        Line line = new Line(ecartw + spacew * step.x1, ecarth + spaceh * step.y1, ecartw + spacew * step.x2, ecarth + spaceh * step.y2);
         line.setStrokeWidth(5);
         step.ln = line;
         gameContext.getChildren().add(line);
     }
 
-    private void button(){
+    private void button() {
         ProgressButton b;
 
         b = new ProgressButton();
         b.setLayoutX(ecartw - radius);
-        b.setLayoutY(1.0/3* ecarth);
+        b.setLayoutY(1.0 / 3 * ecarth);
         b.getButton().setRadius(radius);
         b.getButton().setFill(Color.RED);
         b.assignIndicatorUpdatable(event -> {
             gameContext.getChildren().add(player);
             move(start[0], true);
-            for (ProgressButton p : progressButtons){
+            for (ProgressButton p : progressButtons) {
                 p.disable();
-                if (progressButtons.indexOf(p)==0) {
+                if (progressButtons.indexOf(p) == 0) {
                     p.setOpacity(1);
                 }
             }
@@ -187,15 +188,15 @@ public class Ladder implements GameLifeCycle {
 
         b = new ProgressButton();
         b.setLayoutX(ecartw - radius + spacew);
-        b.setLayoutY(1.0/3* ecarth);
+        b.setLayoutY(1.0 / 3 * ecarth);
         b.getButton().setFill(Color.RED);
         b.getButton().setRadius(radius);
         b.assignIndicatorUpdatable(event -> {
             gameContext.getChildren().add(player);
             move(start[1], true);
-            for (ProgressButton p : progressButtons){
+            for (ProgressButton p : progressButtons) {
                 p.disable();
-                if (progressButtons.indexOf(p)==1) {
+                if (progressButtons.indexOf(p) == 1) {
                     p.setOpacity(1);
                 }
             }
@@ -206,16 +207,16 @@ public class Ladder implements GameLifeCycle {
         gameContext.getGazeDeviceManager().addEventFilter(b);
 
         b = new ProgressButton();
-        b.setLayoutX(ecartw - radius + 2*spacew);
-        b.setLayoutY(1.0/3* ecarth);
+        b.setLayoutX(ecartw - radius + 2 * spacew);
+        b.setLayoutY(1.0 / 3 * ecarth);
         b.getButton().setRadius(radius);
         b.getButton().setFill(Color.RED);
         b.assignIndicatorUpdatable(event -> {
             gameContext.getChildren().add(player);
             move(start[2], true);
-            for (ProgressButton p : progressButtons){
+            for (ProgressButton p : progressButtons) {
                 p.disable();
-                if (progressButtons.indexOf(p)==2) {
+                if (progressButtons.indexOf(p) == 2) {
                     p.setOpacity(1);
                 }
             }
@@ -226,16 +227,16 @@ public class Ladder implements GameLifeCycle {
         gameContext.getGazeDeviceManager().addEventFilter(b);
 
         b = new ProgressButton();
-        b.setLayoutX(ecartw - radius + 3*spacew);
-        b.setLayoutY(1.0/3* ecarth);
+        b.setLayoutX(ecartw - radius + 3 * spacew);
+        b.setLayoutY(1.0 / 3 * ecarth);
         b.getButton().setRadius(radius);
         b.getButton().setFill(Color.RED);
         b.assignIndicatorUpdatable(event -> {
             gameContext.getChildren().add(player);
             move(start[3], true);
-            for (ProgressButton p : progressButtons){
+            for (ProgressButton p : progressButtons) {
                 p.disable();
-                if (progressButtons.indexOf(p)==3) {
+                if (progressButtons.indexOf(p) == 3) {
                     p.setOpacity(1);
                 }
             }
@@ -246,16 +247,16 @@ public class Ladder implements GameLifeCycle {
         gameContext.getGazeDeviceManager().addEventFilter(b);
 
         b = new ProgressButton();
-        b.setLayoutX(ecartw - radius + 4*spacew);
-        b.setLayoutY(1.0/3* ecarth);
+        b.setLayoutX(ecartw - radius + 4 * spacew);
+        b.setLayoutY(1.0 / 3 * ecarth);
         b.getButton().setRadius(radius);
         b.getButton().setFill(Color.RED);
         b.assignIndicatorUpdatable(event -> {
             gameContext.getChildren().add(player);
             move(start[4], true);
-            for (ProgressButton p : progressButtons){
+            for (ProgressButton p : progressButtons) {
                 p.disable();
-                if (progressButtons.indexOf(p)==4) {
+                if (progressButtons.indexOf(p) == 4) {
                     p.setOpacity(1);
                 }
             }
@@ -266,58 +267,51 @@ public class Ladder implements GameLifeCycle {
         gameContext.getGazeDeviceManager().addEventFilter(b);
     }
 
-    private void move(Step step, boolean start){
+    private void move(Step step, boolean start) {
         steps.remove(step);
         fall.remove(step);
         step.ln.setStroke(Color.RED);
-        n = 0;
-        if (step.x1==step.x2){
-            m=15;
+        TranslateTransition wait = new TranslateTransition(Duration.seconds(gameContext.getConfiguration().getAnimationSpeedRatioProperty().doubleValue() / 2), player);
+        wait.setInterpolator(Interpolator.LINEAR);
+        if (start) {
+            wait.setFromX(ecartw + step.x1 * spacew - dimension2D.getHeight() / 20);
+            wait.setFromY(ecarth + step.y1 * spaceh - dimension2D.getHeight() / 20);
+            wait.setToX(ecartw + step.x2 * spacew - dimension2D.getHeight() / 20);
+            wait.setToY(ecarth + step.y2 * spaceh - dimension2D.getHeight() / 20);
         } else {
-            m=100;
+            wait.setToX(ecartw + step.x1 * spacew - dimension2D.getHeight() / 20);
+            wait.setToY(ecarth + step.y1 * spaceh - dimension2D.getHeight() / 20);
+            wait.setFromX(ecartw + step.x2 * spacew - dimension2D.getHeight() / 20);
+            wait.setFromY(ecarth + step.y2 * spaceh - dimension2D.getHeight() / 20);
         }
-        PauseTransition wait = new PauseTransition(Duration.millis(10));
         wait.setOnFinished(event -> {
-            if (n<m){
-                n++;
-                if (start){
-                    player.setX(ecartw + ((step.x2- step.x1)*n/m + step.x1) *spacew - dimension2D.getHeight()/20);
-                    player.setY(ecarth + ((step.y2- step.y1)*n/m + step.y1) *spaceh - dimension2D.getHeight()/20);
-                } else {
-                    player.setX(ecartw + ((step.x2- step.x1)*(m-n)/m + step.x1) *spacew - dimension2D.getHeight()/20);
-                    player.setY(ecarth + ((step.y2- step.y1)*(m-n)/m + step.y1) *spaceh - dimension2D.getHeight()/20);
-                }
-                wait.play();
+            if (start) {
+                find(step.x2, step.y2);
             } else {
-                if (start){
-                    find(step.x2, step.y2);
-                } else {
-                    find (step.x1, step.y1);
-                }
+                find(step.x1, step.y1);
             }
         });
         wait.play();
     }
 
-    private void find(int x, int y){
+    private void find(int x, int y) {
         boolean search = true;
-        for (Step step : steps){
-            if (search && step.x1==x && step.y1==y){
+        for (Step step : steps) {
+            if (search && step.x1 == x && step.y1 == y) {
                 search = false;
                 move(step, true);
-            }
-            else if (search && step.x2==x && step.y2==y){
+            } else if (search && step.x2 == x && step.y2 == y) {
                 search = false;
                 move(step, false);
             }
         }
-        for (Step step : fall){
-            if (search && step.x1==x && step.y1==y){
+        for (Step step : fall) {
+            if (search && step.x1 == x && step.y1 == y) {
                 search = false;
                 move(step, true);
             }
         }
-        if (search){
+        if (search) {
             win();
         }
     }
