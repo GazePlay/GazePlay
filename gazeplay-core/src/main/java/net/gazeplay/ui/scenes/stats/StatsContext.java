6a22117335e412777e7a43319d2b50fb887e6048
karine-benhaimi <ik_benhaimi@esi.dz>
2021-12-23 23:38:33 +0100

    nombre d'erreurs + traduction

--- gazeplay-core/src/main/java/net/gazeplay/ui/scenes/stats/StatsContext.java
+++ gazeplay-core/src/main/java/net/gazeplay/ui/scenes/stats/StatsContext.java
@@ -262,7 +262,7 @@ public class StatsContext extends GraphicalContext<BorderPane> {
                 labelValue = "HiddenItemsShot";
             } else {
                 labelValue = "Score";
-                labelValue1 = "NbErrors";
+                labelValue1 = "nbOfErrors";
                 value1 = new Text(String.valueOf(stats.getNbErrors()));
                 addToGrid(grid, currentFormRow, translator, labelValue1, value1, alignLeft);
             }
