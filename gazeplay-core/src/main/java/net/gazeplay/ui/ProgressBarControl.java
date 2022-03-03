f8d93a5e96582c7a66b4d335691b276a9956f9d7
Yucheng XIAO <saviorseva@gmai.com>
2022-03-03 15:19:16 +0100

    Modified the label for ProgressBarControl

---
+++ gazeplay-core/src/main/java/net/gazeplay/ui/ProgressBarControl.java
@@ -0,0 +1,146 @@
+package net.gazeplay.ui;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.gazeplay.commons.configuration.Configuration;
+import net.gazeplay.commons.ui.I18NTitledPane;
+import net.gazeplay.commons.ui.Translator;
+
+import static net.gazeplay.ui.QuickControl.*;
+
+@NoArgsConstructor
+public class ProgressBarControl {
+
+    @Getter
+    private static final ProgressBarControl instance = new ProgressBarControl();
+
+    public TitledPane createProgressControlPane(Configuration config, Translator translator, Scene primaryScene) {
+        Label progressBarSizeLabel = new Label("");
+        Label colorStringLabel = new Label("Color");
+        Label[] colorLabels = this.createColorLabel(config);
+        progressBarSizeLabel.setMinWidth(ICON_SIZE);
+        Slider progressBarSizeSlider = createProgressBarSizeSlider(config, progressBarSizeLabel);
+
+        HBox line1 = new HBox();
+        line1.setSpacing(CONTENT_SPACING);
+        line1.setAlignment(Pos.CENTER);
+        line1.getChildren().addAll(progressBarSizeLabel, progressBarSizeSlider);
+
+        HBox line2 = new HBox();
+        line2.setSpacing(CONTENT_SPACING);
+        line2.setAlignment(Pos.CENTER);
+        line2.getChildren().add(colorStringLabel);
+        for (Label colorLabel : colorLabels) {
+            line2.getChildren().add(colorLabel);
+        }
+
+        VBox content = new VBox();
+        content.setAlignment(Pos.CENTER);
+        content.setSpacing(CONTENT_SPACING);
+        content.getChildren().addAll(line1, line2);
+        content.setPrefHeight(PREF_HEIGHT);
+
+        I18NTitledPane pane = new I18NTitledPane(translator, "ProgressBarSizeAndColor");
+        pane.setCollapsible(false);
+        pane.setContent(content);
+        return pane;
+    }
+
+    public Slider createProgressBarSizeSlider(Configuration config, Label progressBarSizeLabel) {
+        final int initialSizeValue = config.getProgressBarSize();
+        Slider slider = new Slider();
+        slider.setMinWidth(QuickControl.SLIDERS_MIN_WIDTH);
+        slider.setMaxWidth(QuickControl.SLIDERS_PREF_WIDTH);
+        slider.setPrefWidth(QuickControl.SLIDERS_MAX_WIDTH);
+        slider.setMin(0);
+        slider.setMax(150);
+        slider.setMajorTickUnit(30);
+        slider.setMinorTickCount(10);
+        slider.setShowTickMarks(true);
+        slider.setSnapToTicks(true);
+        slider.setValue(initialSizeValue);
+
+        progressBarSizeLabel.setText(formatValue(initialSizeValue));
+
+        // user can reset ratio to default just by clicking on the label
+        progressBarSizeLabel.setOnMouseClicked(event -> slider.setValue(100));
+
+        slider.valueProperty().addListener((observable) -> {
+            int sizeRatioValue = sliderValueToSizeRatio(slider.getValue());
+            String labelText = formatValue(sizeRatioValue);
+            progressBarSizeLabel.setText(labelText);
+            config.getProgressBarSizeProperty().set(sizeRatioValue);
+        });
+
+        return slider;
+    }
+
+    public void setTextOnLabel(Label[] labels, int position){
+        for(int i=0; i< labels.length; i++){
+            if(i==position) labels[i].setText("⬤");
+            else labels[i].setText("");
+        }
+    }
+
+    public void initializeColorLabel(Label[] labels, int position, String colorName, Configuration config){
+        labels[position].setStyle("-fx-background-color: " + colorName + ";");
+        labels[position].setOnMouseClicked(event -> {
+            config.getProgressBarColorProperty().set(colorName);
+            setTextOnLabel(labels, position);
+        });
+        if(config.getProgressBarColorProperty().getValue().equals(colorName))
+            setTextOnLabel(labels, position);
+    }
+
+    public Label[] createColorLabel(Configuration config){
+        // Sequence : Red Orange Yellow Limegreen Cyan DodgerBlue Violet
+        Label[] labels = new Label[7];
+        for(int i=0; i<labels.length; i++) {
+            labels[i] = new Label();
+            labels[i].setMinSize(25, 25);
+            labels[i].setAlignment(Pos.CENTER);
+        }
+        for(int i=0; i<labels.length; i++){
+            switch(i) {
+                case 0:
+                    initializeColorLabel(labels, 0, "RED", config);
+                    break;
+                case 1:
+                    initializeColorLabel(labels, 1, "ORANGE", config);
+                    break;
+                case 2:
+                    initializeColorLabel(labels, 2, "YELLOW", config);
+                    break;
+                case 3:
+                    initializeColorLabel(labels, 3, "LIMEGREEN", config);
+                    break;
+                case 4:
+                    initializeColorLabel(labels, 4, "CYAN", config);
+                    break;
+                case 5:
+                    initializeColorLabel(labels, 5, "DODGERBLUE", config);
+                    break;
+                default:
+                    initializeColorLabel(labels, 6, "VIOLET", config);
+                    break;
+            }
+        }
+        return labels;
+    }
+
+    public String formatValue(int speedRatioValue) {
+        return speedRatioValue + "%";
+    }
+
+    public int sliderValueToSizeRatio(double value) {
+        return (int)value;
+    }
+}
