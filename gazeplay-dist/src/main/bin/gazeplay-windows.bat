ac13611de5dc113bf7a650e8a15bc8d1df3709d9
Noars <55838787+Noars@users.noreply.github.com>
2022-03-09 11:25:04 +0100

    Change for AFSR version

--- gazeplay-dist/src/main/bin/gazeplay-windows.bat
+++ gazeplay-dist/src/main/bin/gazeplay-windows.bat
@@ -3,6 +3,6 @@
 SET JAVA_HOME=..\lib\jre
 SET PATH=%JAVA_HOME%\bin;%PATH%;%LocalAppData%\TobiiStreamEngineForJava\lib\tobii\x64
 
-java -cp "..\lib\*" -Xms256m -Xmx1g -Dlogging.appender.console.level=OFF net.gazeplay.GazePlayLauncher %*
+java -cp "..\lib\*" -Xms256m -Xmx1g -Dlogging.appender.console.level=OFF net.gazeplay.GazePlayLauncher --afsrgazeplay %*
 
 pause
