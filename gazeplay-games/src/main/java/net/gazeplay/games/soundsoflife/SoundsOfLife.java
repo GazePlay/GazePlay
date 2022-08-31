89cb0acc6df9001a09ce5306f6fffc0e7e744e93
Noars <55838787+Noars@users.noreply.github.com>
2022-08-31 13:52:30 +0200

    Fix sounds of life games

--- gazeplay-games/src/main/java/net/gazeplay/games/soundsoflife/SoundsOfLife.java
+++ gazeplay-games/src/main/java/net/gazeplay/games/soundsoflife/SoundsOfLife.java
@@ -27,175 +27,25 @@ public class SoundsOfLife implements GameLifeCycle {
 
     private final Stats stats;
     private final ReplayablePseudoRandom randomGenerator;
-
+    private final int gameVariant;
     private final IGameContext gameContext;
-
     private final ArrayList<TargetAOI> targetAOIList;
 
     public SoundsOfLife(IGameContext gameContext, Stats stats, int gameVariant) {
-        Dimension2D dimensions = gameContext.getGamePanelDimensionProvider().getDimension2D();
-        Configuration config = gameContext.getConfiguration();
         this.stats = stats;
         this.gameContext = gameContext;
         this.targetAOIList = new ArrayList<>();
+        this.gameVariant = gameVariant;
         this.randomGenerator = new ReplayablePseudoRandom();
         this.stats.setGameSeed(randomGenerator.getSeed());
-
-        String path = "data/soundsoflife/";
-        switch (gameVariant) {
-            case 0:
-                path += "farm/";
-                break;
-            case 1:
-                path += "jungle/";
-                break;
-            case 2:
-                path += "savanna/";
-                break;
-            default:
-                path += "sea/";
-        }
-
-        JsonParser parser = new JsonParser();
-        JsonObject jsonRoot;
-        jsonRoot = (JsonObject) parser.parse(new InputStreamReader(
-            Objects.requireNonNull(ClassLoader.getSystemResourceAsStream(path + "elements.json")), StandardCharsets.UTF_8));
-
-        String backgroundPath = path + jsonRoot.get("background").getAsString();
-        Image backgroundImage = new Image(backgroundPath);
-        ImageView background = new ImageView(backgroundImage);
-
-        // use ratio in order to adapt images to screen
-        double scaleRatio = Math.min(dimensions.getWidth() / backgroundImage.getWidth(),
-            dimensions.getHeight() / backgroundImage.getHeight());
-
-        if (config.isBackgroundEnabled()) {
-            createBackground(background, dimensions, scaleRatio, gameContext);
-        }
-
-        JsonArray elements = jsonRoot.getAsJsonArray("elements");
-        for (JsonElement element : elements) {
-            JsonObject elementObj = (JsonObject) element;
-            // Creating image
-            String imagePath = path + elementObj.get("image").getAsString();
-            Image image = new Image(imagePath);
-            ImageView imageView = new ImageView(image);
-            // Scaling image
-            double scale = elementObj.get("scale").getAsDouble();
-            imageView.setFitWidth(image.getWidth() * scaleRatio * scale);
-            imageView.setFitHeight(image.getHeight() * scaleRatio * scale);
-            // Positioning image
-            JsonObject coordinates = elementObj.getAsJsonObject("coords");
-            double x = coordinates.get("x").getAsDouble() * scaleRatio + background.getX();
-            double y = coordinates.get("y").getAsDouble() * scaleRatio + background.getY();
-            imageView.setX(x - imageView.getFitWidth() / 2);
-            imageView.setY(y - imageView.getFitHeight() / 2);
-            final TargetAOI targetAOI = new TargetAOI(imageView.getX(), y, (int) ((imageView.getFitWidth() + imageView.getFitHeight()) / 3),
-                System.currentTimeMillis());
-            targetAOIList.add(targetAOI);
-            // Creating progress indicator
-            ProgressIndicator progressIndicator = new ProgressIndicator(0);
-            double progIndicSize = Math.min(imageView.getFitWidth(), imageView.getFitHeight()) / 2;
-            progressIndicator.setPrefSize(progIndicSize, progIndicSize);
-            progressIndicator.setLayoutX(x - progIndicSize / 2);
-            progressIndicator.setLayoutY(y - progIndicSize / 2);
-            progressIndicator.setOpacity(0);
-
-            // Listing all the sound paths
-            JsonArray soundPaths = elementObj.getAsJsonArray("sounds");
-            ArrayList<String> sounds = new ArrayList<>();
-            for (JsonElement sound : soundPaths) {
-                sounds.add(path + sound.getAsString());
-            }
-
-            SoundMakingEntity entity = new SoundMakingEntity(imageView, stats, sounds, progressIndicator,
-                gameContext, gameContext.getSoundManager(), randomGenerator);
-            gameContext.getChildren().add(entity);
-            gameContext.getGazeDeviceManager().addEventFilter(entity);
-        }
-
     }
 
     public SoundsOfLife(IGameContext gameContext, Stats stats, int gameVariant, double gameSeed) {
-        Dimension2D dimensions = gameContext.getGamePanelDimensionProvider().getDimension2D();
-        Configuration config = gameContext.getConfiguration();
         this.stats = stats;
         this.gameContext = gameContext;
         this.targetAOIList = new ArrayList<>();
+        this.gameVariant = gameVariant;
         this.randomGenerator = new ReplayablePseudoRandom(gameSeed);
-        String path = "data/soundsoflife/";
-        switch (gameVariant) {
-            case 0:
-                path += "farm/";
-                break;
-            case 1:
-                path += "jungle/";
-                break;
-            case 2:
-                path += "savanna/";
-                break;
-            default:
-                path += "sea/";
-        }
-
-        JsonParser parser = new JsonParser();
-        JsonObject jsonRoot;
-        jsonRoot = (JsonObject) parser.parse(new InputStreamReader(
-            Objects.requireNonNull(ClassLoader.getSystemResourceAsStream(path + "elements.json")), StandardCharsets.UTF_8));
-
-        String backgroundPath = path + jsonRoot.get("background").getAsString();
-        Image backgroundImage = new Image(backgroundPath);
-        ImageView background = new ImageView(backgroundImage);
-
-        // use ratio in order to adapt images to screen
-        double scaleRatio = Math.min(dimensions.getWidth() / backgroundImage.getWidth(),
-            dimensions.getHeight() / backgroundImage.getHeight());
-
-        if (config.isBackgroundEnabled()) {
-            createBackground(background, dimensions, scaleRatio, gameContext);
-        }
-
-        JsonArray elements = jsonRoot.getAsJsonArray("elements");
-        for (JsonElement element : elements) {
-            JsonObject elementObj = (JsonObject) element;
-            // Creating image
-            String imagePath = path + elementObj.get("image").getAsString();
-            Image image = new Image(imagePath);
-            ImageView imageView = new ImageView(image);
-            // Scaling image
-            double scale = elementObj.get("scale").getAsDouble();
-            imageView.setFitWidth(image.getWidth() * scaleRatio * scale);
-            imageView.setFitHeight(image.getHeight() * scaleRatio * scale);
-            // Positioning image
-            JsonObject coordinates = elementObj.getAsJsonObject("coords");
-            double x = coordinates.get("x").getAsDouble() * scaleRatio + background.getX();
-            double y = coordinates.get("y").getAsDouble() * scaleRatio + background.getY();
-            imageView.setX(x - imageView.getFitWidth() / 2);
-            imageView.setY(y - imageView.getFitHeight() / 2);
-            final TargetAOI targetAOI = new TargetAOI(imageView.getX(), y, (int) ((imageView.getFitWidth() + imageView.getFitHeight()) / 3),
-                System.currentTimeMillis());
-            targetAOIList.add(targetAOI);
-            // Creating progress indicator
-            ProgressIndicator progressIndicator = new ProgressIndicator(0);
-            double progIndicSize = Math.min(imageView.getFitWidth(), imageView.getFitHeight()) / 2;
-            progressIndicator.setPrefSize(progIndicSize, progIndicSize);
-            progressIndicator.setLayoutX(x - progIndicSize / 2);
-            progressIndicator.setLayoutY(y - progIndicSize / 2);
-            progressIndicator.setOpacity(0);
-
-            // Listing all the sound paths
-            JsonArray soundPaths = elementObj.getAsJsonArray("sounds");
-            ArrayList<String> sounds = new ArrayList<>();
-            for (JsonElement sound : soundPaths) {
-                sounds.add(path + sound.getAsString());
-            }
-
-            SoundMakingEntity entity = new SoundMakingEntity(imageView, stats, sounds, progressIndicator,
-                gameContext, gameContext.getSoundManager(), randomGenerator);
-            gameContext.getChildren().add(entity);
-            gameContext.getGazeDeviceManager().addEventFilter(entity);
-        }
-
     }
 
     private void createBackground(ImageView background, Dimension2D dimensions, double scaleRatio, IGameContext gameContext) {
@@ -214,6 +64,85 @@ public class SoundsOfLife implements GameLifeCycle {
 
     @Override
     public void launch() {
+
+        gameContext.getChildren().clear();
+
+        Dimension2D dimensions = gameContext.getGamePanelDimensionProvider().getDimension2D();
+        Configuration config = gameContext.getConfiguration();
+
+        String path = "data/soundsoflife/";
+        switch (gameVariant) {
+            case 0:
+                path += "farm/";
+                break;
+            case 1:
+                path += "jungle/";
+                break;
+            case 2:
+                path += "savanna/";
+                break;
+            default:
+                path += "sea/";
+        }
+
+        JsonParser parser = new JsonParser();
+        JsonObject jsonRoot;
+        jsonRoot = (JsonObject) parser.parse(new InputStreamReader(
+            Objects.requireNonNull(ClassLoader.getSystemResourceAsStream(path + "elements.json")), StandardCharsets.UTF_8));
+
+        String backgroundPath = path + jsonRoot.get("background").getAsString();
+        Image backgroundImage = new Image(backgroundPath);
+        ImageView background = new ImageView(backgroundImage);
+
+        // use ratio in order to adapt images to screen
+        double scaleRatio = Math.min(dimensions.getWidth() / backgroundImage.getWidth(),
+            dimensions.getHeight() / backgroundImage.getHeight());
+
+        if (config.isBackgroundEnabled()) {
+            createBackground(background, dimensions, scaleRatio, gameContext);
+        }
+
+        JsonArray elements = jsonRoot.getAsJsonArray("elements");
+        for (JsonElement element : elements) {
+            JsonObject elementObj = (JsonObject) element;
+            // Creating image
+            String imagePath = path + elementObj.get("image").getAsString();
+            Image image = new Image(imagePath);
+            ImageView imageView = new ImageView(image);
+            // Scaling image
+            double scale = elementObj.get("scale").getAsDouble();
+            imageView.setFitWidth(image.getWidth() * scaleRatio * scale);
+            imageView.setFitHeight(image.getHeight() * scaleRatio * scale);
+            // Positioning image
+            JsonObject coordinates = elementObj.getAsJsonObject("coords");
+            double x = coordinates.get("x").getAsDouble() * scaleRatio + background.getX();
+            double y = coordinates.get("y").getAsDouble() * scaleRatio + background.getY();
+            imageView.setX(x - imageView.getFitWidth() / 2);
+            imageView.setY(y - imageView.getFitHeight() / 2);
+            final TargetAOI targetAOI = new TargetAOI(imageView.getX(), y, (int) ((imageView.getFitWidth() + imageView.getFitHeight()) / 3),
+                System.currentTimeMillis());
+            targetAOIList.add(targetAOI);
+            // Creating progress indicator
+            ProgressIndicator progressIndicator = new ProgressIndicator(0);
+            double progIndicSize = Math.min(imageView.getFitWidth(), imageView.getFitHeight()) / 2;
+            progressIndicator.setPrefSize(progIndicSize, progIndicSize);
+            progressIndicator.setLayoutX(x - progIndicSize / 2);
+            progressIndicator.setLayoutY(y - progIndicSize / 2);
+            progressIndicator.setOpacity(0);
+
+            // Listing all the sound paths
+            JsonArray soundPaths = elementObj.getAsJsonArray("sounds");
+            ArrayList<String> sounds = new ArrayList<>();
+            for (JsonElement sound : soundPaths) {
+                sounds.add(path + sound.getAsString());
+            }
+
+            SoundMakingEntity entity = new SoundMakingEntity(imageView, stats, sounds, progressIndicator,
+                gameContext, gameContext.getSoundManager(), randomGenerator);
+            gameContext.getChildren().add(entity);
+            gameContext.getGazeDeviceManager().addEventFilter(entity);
+        }
+
         stats.notifyNewRoundReady();
         gameContext.getGazeDeviceManager().addStats(stats);
         final BackgroundMusicManager backgroundMusicManager = BackgroundMusicManager.getInstance();
