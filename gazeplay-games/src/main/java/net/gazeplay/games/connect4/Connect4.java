e3288a7e5207b9c9c52e9baff747a93c671ed703
HugolinBouhineau <hugolin.bc@gmail.com>
2023-03-09 14:13:43 +0100

    Create Connect4 Skeleton

---
+++ gazeplay-games/src/main/java/net/gazeplay/games/connect4/Connect4.java
@@ -0,0 +1,26 @@
+package net.gazeplay.games.connect4;
+
+import net.gazeplay.GameLifeCycle;
+import net.gazeplay.IGameContext;
+import net.gazeplay.commons.utils.stats.Stats;
+
+public class Connect4 implements GameLifeCycle {
+
+    Connect4(final IGameContext gameContext, final Stats stats){
+
+    }
+
+    Connect4(final IGameContext gameContext, final Stats stats, double gameSeed){
+
+    }
+
+    @Override
+    public void launch() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
