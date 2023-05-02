d0a5e5cf28aaf03f330a25ac94bd72fe00684d23
iwa <nb.boutemeur@gmail.com>
2023-05-02 11:40:05 +0200

    Fix ordre de la sélection des niveaux pour Rush Hour

--- gazeplay-games/src/main/java/net/gazeplay/games/rushhour/RushHourGameVariant.java
+++ gazeplay-games/src/main/java/net/gazeplay/games/rushhour/RushHourGameVariant.java
@@ -17,49 +17,12 @@ public class RushHourGameVariant implements IGameVariantGenerator {
 
         Set<IGameVariant> gameVariants = new HashSet<>();
 
-        for (int i = 1; i <= 33; i++){
+        for (int i = 1; i <= 33; i++) {
             gameVariants.add(new IntGameVariant(i));
         }
 
         return gameVariants.stream()
-            .sorted(Comparator.comparing(IGameVariant::toString))
+            .sorted(Comparator.comparingInt(v -> ((IntGameVariant) v).getNumber()))
             .collect(Collectors.toCollection(LinkedHashSet::new));
-        /*
-        return Sets.newLinkedHashSet(Lists.newArrayList(
-            new IntStringGameVariant(1, "Niveau1-5"),
-            new IntStringGameVariant(2, "Niveau1-5"),
-            new IntStringGameVariant(3, "Niveau1-5"),
-            new IntStringGameVariant(4, "Niveau1-5"),
-            new IntStringGameVariant(5, "Niveau1-5"),
-            new IntStringGameVariant(6, "Niveau6-11"),
-            new IntStringGameVariant(7, "Niveau6-11"),
-            new IntStringGameVariant(8, "Niveau6-11"),
-            new IntStringGameVariant(9, "Niveau6-11"),
-            new IntStringGameVariant(10, "Niveau6-11"),
-            new IntStringGameVariant(11, "Niveau6-11"),
-            new IntStringGameVariant(12, "Niveau12-17"),
-            new IntStringGameVariant(13, "Niveau12-17"),
-            new IntStringGameVariant(14, "Niveau12-17"),
-            new IntStringGameVariant(15, "Niveau12-17"),
-            new IntStringGameVariant(16, "Niveau12-17"),
-            new IntStringGameVariant(17, "Niveau12-17"),
-            new IntStringGameVariant(18, "Niveau18-23"),
-            new IntStringGameVariant(19, "Niveau18-23"),
-            new IntStringGameVariant(20, "Niveau18-23"),
-            new IntStringGameVariant(21, "Niveau18-23"),
-            new IntStringGameVariant(22, "Niveau18-23"),
-            new IntStringGameVariant(23, "Niveau18-23"),
-            new IntStringGameVariant(24, "Niveau24-29"),
-            new IntStringGameVariant(25, "Niveau24-29"),
-            new IntStringGameVariant(26, "Niveau24-29"),
-            new IntStringGameVariant(27, "Niveau24-29"),
-            new IntStringGameVariant(28, "Niveau24-29"),
-            new IntStringGameVariant(29, "Niveau24-29"),
-            new IntStringGameVariant(30, "Niveau30-33"),
-            new IntStringGameVariant(31, "Niveau30-33"),
-            new IntStringGameVariant(32, "Niveau30-33"),
-            new IntStringGameVariant(33, "Niveau30-33")
-
-        ));*/
     }
 }
