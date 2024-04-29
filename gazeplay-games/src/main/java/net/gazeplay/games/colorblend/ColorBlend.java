5dbac37a54344df7a08b0d9aa610df83d2b05a7c
Yaniissou <yanissoumc@gmail.com>
2024-04-29 09:14:06 +0200

    Advanced on ColorBlend game

--- gazeplay-games/src/main/java/net/gazeplay/games/colorblend/ColorBlend.java
+++ gazeplay-games/src/main/java/net/gazeplay/games/colorblend/ColorBlend.java
@@ -3,35 +3,43 @@ package net.gazeplay.games.colorblend;
 import javafx.geometry.Dimension2D;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.shape.Circle;
 import lombok.extern.slf4j.Slf4j;
 import net.gazeplay.GameLifeCycle;
 import net.gazeplay.IGameContext;
 import net.gazeplay.commons.utils.stats.Stats;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Paint;
 import javafx.scene.paint.Color;
 import javafx.geometry.Insets;
 
-import java.awt.*;
-
 /**
  * Choose between multiple colors and blend them together
+ *
  * @author Yanis HARKATI
  */
 @Slf4j
 public class ColorBlend implements GameLifeCycle {
-
     private IGameContext gameContext;
     private Stats stats;
 
-    public ColorBlend(final IGameContext gameContext, Stats stats){
+    private Paint color1;
+    private Paint color2;
+    private Circle circle;
+
+    private static final int CIRCLE_RADIUS = 400;
+    private static final int PALETTE_WIDTH = 180;
+
+    public ColorBlend(final IGameContext gameContext, Stats stats) {
         this.gameContext = gameContext;
         this.stats = stats;
         final Dimension2D dimensions = gameContext.getGamePanelDimensionProvider().getDimension2D();
-        createBackground();
     }
+
     @Override
     public void launch() {
-    createPalette();
+        createBackground();
+        createPalette();
     }
 
     @Override
@@ -39,6 +47,9 @@ public class ColorBlend implements GameLifeCycle {
 
     }
 
+    /**
+     * Apply background
+     */
     private void createBackground() {
         Background background = new Background(new BackgroundImage(
             new Image("data/colorblend/images/park.png"),
@@ -48,8 +59,11 @@ public class ColorBlend implements GameLifeCycle {
         gameContext.getRoot().setBackground(background);
     }
 
-    private void createPalette(){
-        // Création des rectangles pour afficher les couleurs spécifiées
+    /**
+     * Create the palette of colors
+     */
+    private void createPalette() {
+        //Create colors
         Rectangle[] colors = {
             createColorRectangle(Color.RED),
             createColorRectangle(Color.ORANGE),
@@ -63,6 +77,10 @@ public class ColorBlend implements GameLifeCycle {
             createColorRectangle(Color.YELLOW)
         };
 
+        for (Rectangle color : colors) {
+            color.setOnMouseClicked(mouseEvent -> handleMouseClick(color));
+        }
+
         // Création de la grille pour disposer les rectangles par paires
         GridPane colorGrid = new GridPane();
         colorGrid.setPadding(new Insets(20));
@@ -75,18 +93,22 @@ public class ColorBlend implements GameLifeCycle {
         }
 
         // Création du rectangle englobant
-        Rectangle boundingRect = createBoundingRectangle(colors.length / 2);
+        Rectangle paletteRectangle = createPaletteRectangle(colors.length / 2);
 
         // Empilement du rectangle englobant et de la grille dans une StackPane
-        StackPane root = new StackPane(boundingRect, colorGrid);
+        StackPane root = new StackPane(paletteRectangle, colorGrid);
 
         // Création de la scène
-       gameContext.getChildren().add(root);
+        gameContext.getChildren().add(root);
     }
 
-    // Méthode pour créer un rectangle de couleur avec une taille spécifique
+    /**
+     * Create a rectangle for a color
+     * @param color the color used
+     * @return a Rectangle Object used for choosing color
+     */
     private Rectangle createColorRectangle(Color color) {
-        Rectangle rectangle = new Rectangle(50, 50);
+        Rectangle rectangle = new Rectangle(75, 75);
         rectangle.setFill(color);
         rectangle.setStroke(color.darker());
         rectangle.setStrokeWidth(1);
@@ -94,12 +116,65 @@ public class ColorBlend implements GameLifeCycle {
     }
 
 
-    private Rectangle createBoundingRectangle(int numPairs) {
-        double height = numPairs * 53 + (numPairs - 1) * 10; // Hauteur = (hauteur d'un rectangle + espacement) * nombre de paires de couleurs - espacement
-        Rectangle rectangle = new Rectangle(120, height); // Largeur fixe pour plus de visibilité
+    private Rectangle createPaletteRectangle(int numPairs) {
+        double height = numPairs * 53 + (numPairs - 1) * 10; // Hauteur = (height of a color rectangle + spacing) * size of colors - spacing
+        Rectangle rectangle = new Rectangle(PALETTE_WIDTH, height); // constant width for more visibility
         rectangle.setFill(Color.BEIGE);
         rectangle.setStroke(Color.BEIGE.darker());
         rectangle.setStrokeWidth(2);
         return rectangle;
+    }
+
+    private void handleMouseClick(Rectangle color) {
+        if (this.color1 == null) {
+            this.color1 = color.getFill();
+            this.circle = createCirle();
+            this.circle.setFill(this.color1);
+        } else {
+            this.color2 = color.getFill();
+            color.setStroke(Color.WHITE);
+            Color newColor = blendColors();
+            this.circle.setFill(newColor);
+        }
+
+    }
+    /**
+     * Get a blended color from 2 initial colors
+     *
+     * @return a blended Color object
+     */
+    private Color blendColors() {
+        //Get the initial R G B elements of colors
+        Color color1 = (Color) this.color1;
+        Color color2 = (Color) this.color2;
+
+        double r1 = color1.getRed();
+        double g1 = color1.getGreen();
+        double b1 = color1.getBlue();
+
+        double r2 = color2.getRed();
+        double g2 = color2.getGreen();
+        double b2 = color2.getBlue();
+
+        // Merge colors
+        double r = (r1 + r2) / 2;
+        double g = (g1 + g2) / 2;
+        double b = (b1 + b2) / 2;
+
+        return new Color(r,g,b,1);
+    }
+
+    private Circle createCirle(){
+        Circle circle = new Circle();
+        double radius = CIRCLE_RADIUS;
+        circle.setRadius(radius);
+
+        this.gameContext.getRoot().getChildren().add(circle);
+
+        circle.centerXProperty().bind(this.gameContext.getRoot().widthProperty().divide(2));
+        circle.centerYProperty().bind(this.gameContext.getRoot().heightProperty().divide(2));
+
+
+        return circle;
     }
 }
