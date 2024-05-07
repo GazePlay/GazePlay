d267ab7697fd81253b6f8d0a6a304140e8df7212
Yaniissou <yanissoumc@gmail.com>
2024-05-07 10:05:06 +0200

    fixed progress indicator not showing for button

--- gazeplay-games/src/main/java/net/gazeplay/games/colorblend/ColorBlend.java
+++ gazeplay-games/src/main/java/net/gazeplay/games/colorblend/ColorBlend.java
@@ -128,9 +128,9 @@ public class ColorBlend implements GameLifeCycle {
 
 
         // Putting, palette, colorgrid in the same stackpane
-        StackPane root = new StackPane(paletteRectangle, colorGrid, this.progressIndicator);
+        StackPane root = new StackPane(paletteRectangle, colorGrid);
 
-        VBox container = new VBox(root, buttonBox);
+        VBox container = new VBox(root, buttonBox, this.progressIndicator);
         container.setAlignment(Pos.CENTER); // Alignement du conteneur au centre
 
         // Création de la scène
