ad20f3eeabe6b9b3ebe27dfef6d8f087bbbe20fb
Noars <55838787+Noars@users.noreply.github.com>
2026-05-21 17:31:53 +0200

    change affichage options ecran

--- gazeplay-commons/src/main/java/net/gazeplay/commons/configuration/EvalOptions.java
+++ gazeplay-commons/src/main/java/net/gazeplay/commons/configuration/EvalOptions.java
@@ -1,5 +1,20 @@
 package net.gazeplay.commons.configuration;
 
 public enum EvalOptions {
-    Classique, Autonomie_Total, Autonomie_Soutient_Oral, Autonomie_Soutient_Visuel
+
+    Classique("Classique"),
+    Autonomie_Total("Autonomie Total"),
+    Autonomie_Soutient_Oral("Autonomie Soutient Oral"),
+    Autonomie_Soutient_Visuel("Autonomie Soutient Visuel");
+
+    private final String displayName;
+
+    EvalOptions(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
